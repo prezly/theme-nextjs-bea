094e9f07f6cab3eed05f28da800ef3353ca2bc0a
Gijs Nelissen <gijs@prezly.com>
2021-03-23 12:15:24 +0100

    Add eslint + reformat code

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+        'react/jsx-indent': ['error', 4],
+        indent: [
+            'error', 4,
+        ],
+    },
+};
