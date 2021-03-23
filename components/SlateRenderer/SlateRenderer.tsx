5fbbb7301dd42e293e0612624c5c8fcdce80378a
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-03-23 15:22:18 +0100

    Add SlateRenderer component

---
+++ components/SlateRenderer/SlateRenderer.tsx
@@ -0,0 +1,13 @@
+import type { FunctionComponent } from 'react';
+import { Node, Renderer } from '@prezly/slate-renderer';
+import '@prezly/slate-renderer/build/styles.css';
+
+interface Props {
+    nodes: Node | Node[];
+}
+
+const SlateRenderer: FunctionComponent<Props> = ({ nodes }) => (
+    <Renderer nodes={nodes} />
+);
+
+export default SlateRenderer;
