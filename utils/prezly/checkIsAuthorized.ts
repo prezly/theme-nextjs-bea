fac757f9f21e1fe940ecda99f5e1a16a38bbbb38
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-03-23 15:22:45 +0100

    Add "checkIsAuthorized" util

---
+++ utils/prezly/checkIsAuthorized.ts
@@ -0,0 +1,28 @@
+import { IncomingMessage } from 'http';
+
+const checkIsAuthorized = (req: IncomingMessage) => {
+    if (process.browser) {
+        throw new Error('"checkIsAuthorized" should only be used on back-end side.');
+    }
+
+    const secret = process.env.HTTP_ENV_SECRET;
+    const authorization = req!.headers.authorization;
+
+    if (!secret) {
+        return true;
+    }
+
+    if (!authorization || !req) {
+        return false;
+    }
+
+    const [authorizationType, credentials] = authorization.split(' ');
+
+    if (authorizationType.toLowerCase() === 'bearer') {
+        return credentials === secret;
+    }
+
+    throw new Error(`Unknown authorization type "${authorizationType}"`);
+};
+
+export default checkIsAuthorized;
