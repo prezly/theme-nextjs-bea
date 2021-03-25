6fd198e4ba3864436e806453a0da1249b5e3444d
Sato <sato.rc@yandex.ru>
2021-03-25 13:04:45 +0300

    Replace `eslint-config-airbnb` with `eslint-config-airbnb-typescript`

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     extends: [
         'plugin:react/recommended',
-        'airbnb',
+        'airbnb-typescript',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
