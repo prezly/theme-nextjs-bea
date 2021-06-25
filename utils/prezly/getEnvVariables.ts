45490411307ecb618c9b1bbfb41092abfd8bc0bb
Ivan Voskoboinyk <ivan@prezly.com>
2021-06-25 13:50:27 +0300

    Add support for data-uris in the HTTP env header

--- utils/prezly/getEnvVariables.ts
+++ utils/prezly/getEnvVariables.ts
@@ -1,20 +1,40 @@
+import parseDataUrl from 'parse-data-url';
 import type { IncomingMessage } from 'http';
 import type { Env } from '../../types';
+
+const decodeJson = (json: string): Record<string, any> => {
+    try {
+        const decoded = JSON.parse(json);
+        if (decoded && typeof decoded === 'object') {
+            return decoded;
+        }
+    } catch {
+        // passthru
+    }
+    return {};
+};
+
+const decodeHttpEnv = (header: string): Record<string, any> => {
+    if (header.startsWith('data:')) {
+        const parsed = parseDataUrl(header);
+        if (parsed && parsed.contentType === 'application/json') {
+            return decodeJson(parsed.data);
+        }
+        return {}; // unsupported data-uri
+    }
+    return decodeJson(header);
+};
 
 const getEnvVariables = (req?: IncomingMessage): Env => {
     if (process.browser) {
         throw new Error('"getEnvVariables" should only be used on back-end side.');
     }
 
-    const envHeader = (process.env.HTTP_ENV_HEADER || '').toLowerCase();
+    const httpEnvHeader = (process.env.HTTP_ENV_HEADER || '').toLowerCase();
 
-    if (envHeader && req) {
-        try {
-            const envJson = req.headers[envHeader] as string;
-            return { ...process.env, ...JSON.parse(envJson) };
-        } catch {
-            // do nothing
-        }
+    if (httpEnvHeader && req) {
+        const httpEnv = decodeHttpEnv(httpEnvHeader);
+        return { ...process.env, ...httpEnv };
     }
 
     return { ...process.env };
