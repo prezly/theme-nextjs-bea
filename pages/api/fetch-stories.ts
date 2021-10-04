ce0dff377a47bd32c63e03b5731a585923ef232d
Lukas Kudlik <kudlajzzz@gmail.com>
2021-10-04 12:46:47 +0200

    Added type cast to error object

--- pages/api/fetch-stories.ts
+++ pages/api/fetch-stories.ts
@@ -18,9 +18,9 @@ export default async function fetchStories(req: NextApiRequest, res: NextApiResp
         );
 
         res.status(200).json({ stories });
-    } catch (err) {
+    } catch (error) {
         res.status(500).send({
-            message: err.message,
+            message: (error as Error).message,
         });
     }
 }
