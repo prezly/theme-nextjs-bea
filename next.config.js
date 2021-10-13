dccf6b0e30130b1a6065f35a8c83364ccac76943
Oleg Semyonov <oleg@prezly.com>
2021-10-13 12:31:26 +0300

    Add svgr to the webpack config, lint fixes for next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          }
-        ],
-      },
-    ]
-  },
-  images: {
-    domains: ['cdn.uc.assets.prezly.com'],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=0, must-revalidate',
+                    },
+                ],
+            },
+        ];
+    },
+    images: {
+        domains: ['cdn.uc.assets.prezly.com'],
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
