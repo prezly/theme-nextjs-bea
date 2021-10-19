e9c4bf1b2348f249b604cb899e0be5ec3a07ea57
Oleg Semyonov <oleg@prezly.com>
2021-10-19 17:40:05 +0300

    Improve typings for SVG

---
+++ custom-next-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// Using custom env file is neccessary to override `any` type for imported SVGs
+// See: https://duncanleung.com/next-js-typescript-svg-any-module-declaration/
