58b18a5df1680e6b24bd6176fb5c08fb41468cbe
Lukas Kudlik <kudlajzzz@gmail.com>
2021-10-19 21:27:42 +0200

    Added new icons for quotes and attachments

--- icons/index.ts
+++ icons/index.ts
@@ -1,3 +1,7 @@
+export * from './file-types';
 export * from './social';
 export { default as IconMenu } from './menu.svg';
+export { default as IconPaste } from './paste.svg';
+export { default as IconQuoteClosing } from './quote_closing.svg';
+export { default as IconQuoteOpening } from './quote_opening.svg';
 export { default as LogoPrezly } from './prezly.svg';
