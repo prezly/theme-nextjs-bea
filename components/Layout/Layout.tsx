ff94699d72f41769ed38f4b7580dbde75f12a476
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-20 15:02:38 +0200

    Update layout styling and add loading indicator

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,43 @@
-import { FunctionComponent } from 'react';
+import classNames from 'classnames';
+import { Router } from 'next/router';
+import { FunctionComponent, useEffect, useState } from 'react';
+
+import LoadingIndicator from '../LoadingIndicator';
 
 import Boilerplate from './Boilerplate';
 import Footer from './Footer';
 import Header from './Header';
 
-const Layout: FunctionComponent = ({ children }) => (
-    <>
-        <Header />
-        <div className="container">{children}</div>
-        <Boilerplate />
-        <Footer />
-    </>
-);
+import styles from './Layout.module.scss';
 
+const Layout: FunctionComponent = ({ children }) => {
+    const [isLoadingPage, setIsLoadingPage] = useState(false);
+
+    useEffect(() => {
+        const onRouteChangeStart = () => {
+            setIsLoadingPage(true);
+        };
+
+        Router.events.on('routeChangeStart', onRouteChangeStart);
+        return () => {
+            Router.events.off('routeChangeStart', onRouteChangeStart);
+        };
+    }, []);
+
+    return (
+        <div className={styles.layout}>
+            <Header />
+            <main
+                className={classNames(styles.content, {
+                    [styles.loading]: isLoadingPage,
+                })}
+            >
+                {isLoadingPage && <LoadingIndicator />}
+                {!isLoadingPage && children}
+            </main>
+            <Boilerplate />
+            <Footer />
+        </div>
+    );
+};
 export default Layout;
