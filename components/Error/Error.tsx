517b8f562ef35774ca1e7fc4de616ffa7e3a611f
Yuriy Yakym <yakym.yuriy@gmail.com>
2021-10-22 18:14:29 +0200

    Add Error component styling

--- components/Error/Error.tsx
+++ components/Error/Error.tsx
@@ -1,20 +1,22 @@
+import classNames from 'classnames';
 import React, { FunctionComponent, ReactNode } from 'react';
 
 import styles from './Error.module.scss';
 
 interface Props {
     action?: ReactNode;
+    className?: string;
     description?: string;
     statusCode: number;
     title: string;
 }
 
-const Error: FunctionComponent<Props> = ({ action, description, statusCode, title }) => (
-    <div className={styles.error}>
-        <h1>{statusCode}</h1>
-        <h2>{title}</h2>
-        {description && <p>{description}</p>}
-        {action}
+const Error: FunctionComponent<Props> = ({ action, className, description, statusCode, title }) => (
+    <div className={classNames(styles.error, className)}>
+        <div className={styles.statusCode}>{statusCode}</div>
+        <h1 className={styles.title}>{title}</h1>
+        {description && <div className={styles.description}>{description}</div>}
+        {action && <div className={styles.action}>{action}</div>}
     </div>
 );
 
