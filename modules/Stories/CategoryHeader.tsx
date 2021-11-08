927f21abf63bbc2a7b315903538377b39b7b981b
Lukas Kudlik <kudlajzzz@gmail.com>
2021-11-08 17:50:26 +0100

    Use PageTitle in CategoryHeader

--- modules/Stories/CategoryHeader.tsx
+++ modules/Stories/CategoryHeader.tsx
@@ -1,10 +1,9 @@
 import { Category } from '@prezly/sdk';
 import type { FunctionComponent } from 'react';
 
+import PageTitle from '@/components/PageTitle';
 import { useCurrentLocale } from '@/hooks/useCurrentLocale';
 import { getLocalizedCategoryData } from '@/utils/prezly';
-
-import styles from './CategoryHeader.module.scss';
 
 type Props = {
     category: Category;
@@ -14,12 +13,7 @@ const CategoryHeader: FunctionComponent<Props> = ({ category }) => {
     const currentLocale = useCurrentLocale();
     const { name, description } = getLocalizedCategoryData(category, currentLocale);
 
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>{name}</h1>
-            {description && <p className={styles.description}>{description}</p>}
-        </div>
-    );
+    return <PageTitle title={name} subtitle={description} />;
 };
 
 export default CategoryHeader;
