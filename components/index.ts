31ed66f6059a467ef752599dd1a8517e7e4cef60
Lukas Kudlik <kudlajzzz@gmail.com>
2021-11-10 11:49:43 +0100

    Extract common components

--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,5 @@
 export { default as Button } from './Button';
+export { default as LoadingBar } from './LoadingBar';
+export * from './seo';
+export * from './StoryCards';
 export { default as PageTitle } from './PageTitle';
