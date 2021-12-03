d57f0341b4b0ddb6a57d238db0e6fe0f90a5333c
Oleg Semyonov <oleg@prezly.com>
2021-12-03 15:31:20 +0300

    Add missing locale parameters to the "Back to Homepage" link

--- modules/Errors/NotFound/NotFound.tsx
+++ modules/Errors/NotFound/NotFound.tsx
@@ -1,28 +1,28 @@
 import translations from '@prezly/themes-intl-messages';
 import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
 import React, { FunctionComponent } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import Button from '@/components/Button';
 import Error from '@/components/Error';
+import { useGetLinkLocale } from '@/hooks/useGetLinkLocale';
 
 import styles from './NotFound.module.scss';
 
 const Layout = dynamic(() => import('@/modules/Layout'), { ssr: true });
 
 const NotFound: FunctionComponent = () => {
-    const router = useRouter();
     const { formatMessage } = useIntl();
+    const getLinkLocale = useGetLinkLocale();
 
     return (
         <Layout>
             <Error
                 className={styles.error}
                 action={
-                    <Button onClick={() => router.push('/')} variation="primary">
+                    <Button.Link href="/" localeCode={getLinkLocale()} variation="primary">
                         <FormattedMessage {...translations.actions.backToHomePage} />
-                    </Button>
+                    </Button.Link>
                 }
                 statusCode={404}
                 title={formatMessage(translations.notFound.title)}
