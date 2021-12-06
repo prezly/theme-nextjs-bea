d059885c2c1b0975dea5a6b64415cf6d4a6abd6d
Oleg Semyonov <oleg@prezly.com>
2021-12-06 13:18:52 +0300

    Refactor locale-related hooks to split the business logic

--- hooks/useGetLinkLocale.ts
+++ hooks/useGetLinkLocale.ts
@@ -1,3 +1,4 @@
+import type { NewsroomLanguageSettings } from '@prezly/sdk';
 import { useCallback } from 'react';
 
 import { toUrlSlug } from '@/utils/locale';
@@ -6,27 +7,24 @@ import { getShortestLocaleCode } from '@/utils/prezly/api/languages';
 import { useCurrentLocale } from './useCurrentLocale';
 import { useLanguages } from './useLanguages';
 
+// Determine correct locale for the link (trying to shorten the locale code if possible)
+function getLinkLocale(languages: NewsroomLanguageSettings[], localeCode: string) {
+    const shortestLocaleCode = getShortestLocaleCode(languages, localeCode);
+    // When navigating to default language, we don't append the locale to the URL.
+    if (!shortestLocaleCode) {
+        return shortestLocaleCode;
+    }
+
+    return toUrlSlug(shortestLocaleCode);
+}
+
 export function useGetLinkLocale() {
     const currentLocale = useCurrentLocale();
     const languages = useLanguages();
 
-    // Determine correct URL for translated stories/categories with a fallback to homepage
     // The `localeCode` is mainly used for Language Dropdown
-    const getLinkLocale = useCallback(
-        (localeCode?: string) => {
-            const shortestLocaleCode = getShortestLocaleCode(
-                languages,
-                localeCode || currentLocale,
-            );
-            // When navigating to default language, we don't append the locale to the URL.
-            if (!shortestLocaleCode) {
-                return shortestLocaleCode;
-            }
-
-            return toUrlSlug(shortestLocaleCode);
-        },
+    return useCallback(
+        (localeCode?: string) => getLinkLocale(languages, localeCode || currentLocale),
         [languages, currentLocale],
     );
-
-    return getLinkLocale;
 }
--- modules/Layout/Header/LanguagesDropdown/lib/useGetTranslationUrl.ts
+++ modules/Layout/Header/LanguagesDropdown/lib/useGetTranslationUrl.ts
@@ -1,3 +1,4 @@
+import type { Category, Story } from '@prezly/sdk';
 import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 
@@ -5,37 +6,43 @@ import { useSelectedCategory } from '@/hooks/useSelectedCategory';
 import { useSelectedStory } from '@/hooks/useSelectedStory';
 import { getCategoryHasTranslation, getCategoryUrl } from '@/utils/prezly';
 
+// Determine correct URL for translated stories/categories with a fallback to homepage
+function getTranslationUrl(
+    localeCode: string,
+    path: string,
+    selectedCategory?: Category,
+    selectedStory?: Story,
+) {
+    if (selectedCategory) {
+        if (getCategoryHasTranslation(selectedCategory, localeCode)) {
+            return getCategoryUrl(selectedCategory, localeCode);
+        }
+
+        return '/';
+    }
+
+    if (selectedStory && selectedStory.culture.locale !== localeCode) {
+        const translatedStory = selectedStory.translations.find(
+            ({ culture }) => culture.locale === localeCode,
+        );
+        if (translatedStory) {
+            return `/${translatedStory.slug}`;
+        }
+
+        return '/';
+    }
+
+    return path;
+}
+
 export default function useGetTranslationUrl() {
     const { asPath } = useRouter();
     const selectedCategory = useSelectedCategory();
     const selectedStory = useSelectedStory();
 
-    // Determine correct URL for translated stories/categories with a fallback to homepage
-    const getTranslationUrl = useCallback(
-        (localeCode: string) => {
-            if (selectedCategory) {
-                if (getCategoryHasTranslation(selectedCategory, localeCode)) {
-                    return getCategoryUrl(selectedCategory, localeCode);
-                }
-
-                return '/';
-            }
-
-            if (selectedStory && selectedStory.culture.locale !== localeCode) {
-                const translatedStory = selectedStory.translations.find(
-                    ({ culture }) => culture.locale === localeCode,
-                );
-                if (translatedStory) {
-                    return `/${translatedStory.slug}`;
-                }
-
-                return '/';
-            }
-
-            return asPath;
-        },
+    return useCallback(
+        (localeCode: string) =>
+            getTranslationUrl(localeCode, asPath, selectedCategory, selectedStory),
         [asPath, selectedCategory, selectedStory],
     );
-
-    return getTranslationUrl;
 }
