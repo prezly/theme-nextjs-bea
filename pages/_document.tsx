88ff0ecdf4520f5fd481705e4abf2ebb3fdc5ed0
Oleg Semyonov <oleg@prezly.com>
2021-12-13 12:35:45 +0300

    Remove redundant `getInitialProps` method from `_document`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,10 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 import { getLocaleDirection } from '@/utils/locale';
 import { LocaleObject } from '@/utils/localeObject';
+import { BasePageProps } from 'types';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
-
     render() {
         // eslint-disable-next-line no-underscore-dangle
         const { localeCode } = this.props.__NEXT_DATA__.props.pageProps;
