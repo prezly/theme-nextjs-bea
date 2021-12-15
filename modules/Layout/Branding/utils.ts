b97a42201102e0866a6f7250e415fe59f4e5c59e
Lukas Kudlik <kudlajzzz@gmail.com>
2021-12-15 17:36:25 +0100

    Use light or dark button text depending on accent color brightness

--- modules/Layout/Branding/utils.ts
+++ modules/Layout/Branding/utils.ts
@@ -1,6 +1,8 @@
 import { NewsroomThemePreset } from '@prezly/sdk';
 import tinycolor from 'tinycolor2';
 
+const LIGHT_TEXT_COLOR = '#ffffff';
+const DARK_TEXT_COLOR = '#374151';
 const ACCENT_COLOR = '#2563eb';
 const ACCENT_COLOR_TINT_FACTOR = 15;
 const ACCENT_COLOR_SHADE_FACTOR = 20;
@@ -8,6 +10,9 @@ const ACCENT_COLOR_SHADE_FACTOR = 20;
 export const getCssVariables = (themePreset: NewsroomThemePreset) => {
     // TODO: Get values from themePreset when API returns them correctly
     const { editable_settings } = themePreset;
+    const accentColorButtonText = tinycolor(ACCENT_COLOR).isLight()
+        ? DARK_TEXT_COLOR
+        : LIGHT_TEXT_COLOR;
 
     return [
         `--prezly-accent-color: ${ACCENT_COLOR}`,
@@ -17,5 +22,6 @@ export const getCssVariables = (themePreset: NewsroomThemePreset) => {
         `--prezly-accent-color-shade: ${tinycolor(ACCENT_COLOR)
             .darken(ACCENT_COLOR_SHADE_FACTOR)
             .toHexString()}`,
+        `--prezly-accent-color-button-text: ${accentColorButtonText}`,
     ];
 };
