17b421c3fc67652ca60b56b949458ef660eb133b
Lukas Kudlik <kudlajzzz@gmail.com>
2021-12-15 17:31:02 +0100

    Added tint and shade variables for accent color

--- modules/Layout/Branding/utils.ts
+++ modules/Layout/Branding/utils.ts
@@ -1,13 +1,21 @@
 import { NewsroomThemePreset } from '@prezly/sdk';
+import tinycolor from 'tinycolor2';
+
+const ACCENT_COLOR = '#2563eb';
+const ACCENT_COLOR_TINT_FACTOR = 15;
+const ACCENT_COLOR_SHADE_FACTOR = 20;
 
 export const getCssVariables = (themePreset: NewsroomThemePreset) => {
     // TODO: Get values from themePreset when API returns them correctly
     const { editable_settings } = themePreset;
 
-    // return ['--prezly-accent-color: #2563eb'];
     return [
-        '--prezly-accent-color: #ff0000',
-        '--prezly-accent-color-tint: #ff0000',
-        '--prezly-accent-color-shade: #ff0000',
+        `--prezly-accent-color: ${ACCENT_COLOR}`,
+        `--prezly-accent-color-tint: ${tinycolor(ACCENT_COLOR)
+            .lighten(ACCENT_COLOR_TINT_FACTOR)
+            .toHexString()}`,
+        `--prezly-accent-color-shade: ${tinycolor(ACCENT_COLOR)
+            .darken(ACCENT_COLOR_SHADE_FACTOR)
+            .toHexString()}`,
     ];
 };
