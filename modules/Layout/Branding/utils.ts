9a6eb65c3f5eca3e29a9d29f94582a9aa61d00ae
Lukas Kudlik <kudlajzzz@gmail.com>
2021-12-15 17:40:40 +0100

    Mark parameter as unused for now

--- modules/Layout/Branding/utils.ts
+++ modules/Layout/Branding/utils.ts
@@ -7,8 +7,7 @@ const ACCENT_COLOR = '#2563eb';
 const ACCENT_COLOR_TINT_FACTOR = 15;
 const ACCENT_COLOR_SHADE_FACTOR = 20;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getCssVariables = (themePreset: NewsroomThemePreset) => {
+export const getCssVariables = (_themePreset: NewsroomThemePreset) => {
     // TODO: Get values from themePreset when API returns them correctly
     const accentColorButtonText = tinycolor(ACCENT_COLOR).isLight()
         ? DARK_TEXT_COLOR
