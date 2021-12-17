c68d1a11c62cbba5fbc013553ea521bf8e4ad685
Lukas Kudlik <kudlajzzz@gmail.com>
2021-12-17 08:58:33 +0100

    Reduce tint/shade factors

--- modules/Layout/Branding/utils.ts
+++ modules/Layout/Branding/utils.ts
@@ -4,8 +4,8 @@ import tinycolor from 'tinycolor2';
 const LIGHT_TEXT_COLOR = '#ffffff';
 const DARK_TEXT_COLOR = '#374151';
 const ACCENT_COLOR = '#2563eb';
-const ACCENT_COLOR_TINT_FACTOR = 15;
-const ACCENT_COLOR_SHADE_FACTOR = 20;
+const ACCENT_COLOR_TINT_FACTOR = 10;
+const ACCENT_COLOR_SHADE_FACTOR = 10;
 
 export const getCssVariables = (
     _themePreset: NewsroomThemePreset,
