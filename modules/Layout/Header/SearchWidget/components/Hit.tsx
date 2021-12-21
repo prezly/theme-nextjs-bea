99e5d88f811301d95df660d69be42af2bfcf8909
Oleg Semyonov <oleg@prezly.com>
2021-12-21 12:14:02 +0300

    Add links to the story search results

--- modules/Layout/Header/SearchWidget/components/Hit.tsx
+++ modules/Layout/Header/SearchWidget/components/Hit.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import type { Hit } from 'react-instantsearch-core';
 import { Highlight } from 'react-instantsearch-dom';
@@ -15,18 +16,20 @@ const HitComponent: FunctionComponent<Props> = ({ hit }) => {
     const { attributes: story } = hit;
 
     return (
-        <div className={styles.container}>
-            <div className={styles.imageWrapper}>
-                <StoryImage
-                    story={story}
-                    className={styles.image}
-                    placeholderClassName={styles.placeholder}
-                />
-            </div>
-            <p className={styles.title}>
-                <Highlight hit={hit} attribute="attributes.title" tagName="mark" />
-            </p>
-        </div>
+        <Link href={`/${story.slug}`} locale={false} passHref>
+            <a className={styles.container}>
+                <div className={styles.imageWrapper}>
+                    <StoryImage
+                        story={story}
+                        className={styles.image}
+                        placeholderClassName={styles.placeholder}
+                    />
+                </div>
+                <p className={styles.title}>
+                    <Highlight hit={hit} attribute="attributes.title" tagName="mark" />
+                </p>
+            </a>
+        </Link>
     );
 };
 
