5d3b3d8c4bb6dce982318f29ea7e425722e957d4
Camil Blanaru <camil@edka.io>
2021-12-27 14:16:24 +0100

    Set security headers

--- next.config.js
+++ next.config.js
@@ -19,6 +19,26 @@ const moduleExports = {
                         key: 'Cache-Control',
                         value: 'public, max-age=0, must-revalidate',
                     },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Content-Security-Policy',
+                        value: 'upgrade-insecure-requests; report-uri https://prezly.report-uri.com/r/d/csp/enforce;'
+                    }
                 ],
             },
         ];
