1d7bd0ebd9aced6ef73de82bd3b6d0b02f1a78a9
Oleg Semyonov <oleg@prezly.com>
2022-01-21 14:02:49 +0300

    ESLint fixes

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,8 @@
 export { default as capitaliseFirstLetter } from './capitaliseFirstLetter';
 export * from './getAbsoluteUrl';
+export * from './getTypedKeys';
 export * from './lang';
 export * from './locale';
 export * from './localeObject';
 export { default as makeComposableComponent } from './makeComposableComponent';
 export { default as stripHtmlTags } from './stripHtmlTags';
-export * from './getTypedKeys';
