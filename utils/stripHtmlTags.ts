0bdf627789c1931a971472f27065c154d62f562b
Lukas Kudlik <kudlajzzz@gmail.com>
2022-02-01 14:57:50 +0100

    Properly strip HTML tags from boilerplate

--- utils/stripHtmlTags.ts
+++
@@ -1,9 +0,0 @@
-export default function stripHtmlTags(html: string) {
-    if (typeof document !== 'undefined') {
-        const div = document.createElement('div');
-        div.innerHTML = html;
-        return div.textContent || div.innerText || '';
-    }
-
-    return html;
-}
