3c2d693f912495eece1f113f5ee90a881a3ed902
Oleg Semyonov <oleg@prezly.com>
2022-02-02 15:02:05 +0300

    Use preview version of `@prezly/eslint-config` package, remove redundant rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,6 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'airbnb-typescript',
-        'prettier',
-        'next',
-        'next/core-web-vitals',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-        project: ['./tsconfig.json'],
-    },
-    plugins: [
-        '@typescript-eslint',
-        'prettier',
-        'react',
-        'import',
-        'react-hooks',
-        'sort-export-all',
-    ],
+    extends: ['@prezly', '@prezly/eslint-config/react', 'next', 'next/core-web-vitals'],
     rules: {
-        // Prettier handles indent, whitespace and empty lines
-        'prettier/prettier': 2,
-
-        // Import rules
-        'sort-export-all/sort-export-all': [
-            'error',
-            'asc',
-            {
-                caseSensitive: false,
-                natural: false,
-            },
-        ],
         'import/extensions': ['error', 'never', { svg: 'always', json: 'always', css: 'always' }],
         'import/order': [
             'error',
@@ -83,17 +42,6 @@ module.exports = {
                         group: 'internal',
                     },
                 ],
-            },
-        ],
-        'import/prefer-default-export': 'off',
-        'import/no-extraneous-dependencies': 'error',
-        'sort-imports': [
-            'error',
-            {
-                ignoreCase: true,
-                ignoreDeclarationSort: true,
-                ignoreMemberSort: false,
-                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
             },
         ],
 
@@ -139,30 +87,11 @@ module.exports = {
                 trailingUnderscore: 'forbid',
             },
         ],
-        'id-blacklist': [2, 'arr', 'cb', 'e', 'el', 'err', 'idx', 'num', 'str', 'tmp', 'val'],
-        'no-return-assign': ['error', 'except-parens'],
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-            'error',
-            { argsIgnorePattern: '^_', ignoreRestSiblings: true },
-        ],
+        // Allow exporting { default }
+        'no-restricted-exports': 'warn',
 
-        'prefer-destructuring': 'warn',
-        'no-nested-ternary': 'warn',
-
-        // React and a11y specific rules
-        'react/no-unescaped-entities': 'warn',
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off',
-        'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
-        'react/jsx-wrap-multilines': ['error', { arrow: true, return: true, declaration: true }],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
         // next/links breaks this rule
         'jsx-a11y/anchor-is-valid': 'off',
-        'jsx-a11y/alt-text': 'warn',
-        'jsx-a11y/click-events-have-key-events': 'warn',
-        'jsx-a11y/control-has-associated-label': 'warn',
         'jsx-a11y/label-has-associated-control': [
             'warn',
             {
@@ -170,16 +99,8 @@ module.exports = {
                 controlComponents: ['Field'],
             },
         ],
-        'jsx-a11y/iframe-has-title': 'warn',
-        'jsx-a11y/no-autofocus': 'warn',
-        'jsx-a11y/no-noninteractive-element-interactions': 'warn',
-        'jsx-a11y/no-noninteractive-element-to-interactive-role': 'warn',
-        'jsx-a11y/no-static-element-interactions': 'warn',
-        'jsx-a11y/role-has-required-aria-props': 'warn',
 
         // Extra rules
-        radix: 'off',
-        'react/require-default-props': 'off',
         'react/jsx-props-no-spreading': [
             'error',
             {
