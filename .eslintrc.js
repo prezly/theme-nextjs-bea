14a60880a8be3da6f34cbf1abb8c83d8cead3cd4
Oleg Semyonov <oleg@prezly.com>
2022-02-03 14:01:45 +0300

    Update code-style package, disable default export rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
     extends: ['@prezly', '@prezly/eslint-config/react', '@prezly/eslint-config/nextjs'],
     rules: {
+        'import/no-default-export': 'off',
+        'no-restricted-exports': 'off',
+
         'jsx-a11y/label-has-associated-control': [
             'warn',
             {
