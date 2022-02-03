e11096fa042293b539a0ff5a1761c607babfb47a
Oleg Semyonov <oleg@prezly.com>
2022-02-03 17:43:09 +0300

    Add issue link to the eslint comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     rules: {
         // TODO: NextJS Dynamic import doesn't work too well with named exports.
         // Gotta figure out how to make them work together.
+        // Some hints here: https://github.com/vercel/next.js/issues/22278#issuecomment-1009865850
         'import/no-default-export': 'off',
         'no-restricted-exports': 'off',
 
