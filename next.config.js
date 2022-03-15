720cb90751e526860b605dfe770332e351c57d53
Lukas Kudlik <kudlajzzz@gmail.com>
2022-03-15 14:43:54 +0100

    Introduce NEXT_PUBLIC_TRACKING_ENABLED variable

--- next.config.js
+++ next.config.js
@@ -43,6 +43,9 @@ const moduleExports = {
             },
         ];
     },
+    env: {
+        NEXT_PUBLIC_TRACKING_ENABLED: JSON.parse(process.env.NEXT_PUBLIC_TRACKING_ENABLED || true),
+    },
     images: {
         domains: ['cdn.uc.assets.prezly.com'],
     },
