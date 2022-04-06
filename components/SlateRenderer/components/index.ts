074ef5b5d0aecee6fc33811cd20f212dcfe8b7f6
Oleg Semyonov <oleg@prezly.com>
2022-04-06 10:16:52 +0300

    Add support for Placeholder nodes

--- components/SlateRenderer/components/index.ts
+++ components/SlateRenderer/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Attachment } from './Attachment';
 export { default as Gallery } from './Gallery';
 export { default as Image } from './Image';
+export { default as Placeholder } from './Placeholder';
