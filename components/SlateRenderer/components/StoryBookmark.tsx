23515074795d295213fc13a2ddace979c58e65e0
Dmitrii Pikulin <aspirisen@gmail.com>
2022-05-03 22:03:14 +0400

    Use new prop

--- components/SlateRenderer/components/StoryBookmark.tsx
+++ components/SlateRenderer/components/StoryBookmark.tsx
@@ -1,17 +1,18 @@
 import { Elements } from '@prezly/content-renderer-react-js';
 import type { StoryBookmarkNode } from '@prezly/slate-types';
-import { useEmbedStory } from '@prezly/theme-kit-nextjs';
+import { useCurrentStory } from '@prezly/theme-kit-nextjs';
 
 interface Props {
     node: StoryBookmarkNode;
 }
 
 export function StoryBookmark({ node }: Props) {
-    const embedStory = useEmbedStory(node.story.uuid);
+    const story = useCurrentStory();
+    const storyOEmbedInfo = story?.referenced_entities?.stories?.[node.story.uuid];
 
-    if (!embedStory) {
+    if (!storyOEmbedInfo) {
         return null;
     }
 
-    return <Elements.StoryBookmark node={node} story={embedStory} />;
+    return <Elements.StoryBookmark node={node} storyOEmbedInfo={storyOEmbedInfo} />;
 }
