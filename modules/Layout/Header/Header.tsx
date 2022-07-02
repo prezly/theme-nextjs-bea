c705c2bdf6c68e4526f53f9e43d3e4c713359d68
Gijs Nelissen <gijs@prezly.com>
2022-07-02 15:54:16 +0200

    Update mobile header

--- modules/Layout/Header/Header.tsx
+++ modules/Layout/Header/Header.tsx
@@ -93,7 +93,7 @@ const blogPosts = [
 
 
 export default function Example() {
-    const { newsroom_logo, display_name } = useNewsroom();
+    const { newsroom_logo, display_name, square_logo } = useNewsroom();
     const { name } = useCompanyInformation();
     const { ALGOLIA_API_KEY } = useAlgoliaSettings();
     const getLinkLocaleSlug = useGetLinkLocaleSlug();
@@ -343,11 +343,21 @@ export default function Example() {
                         <div className="pt-5 pb-6 px-5 sm:pb-8">
                             <div className="flex items-center justify-between">
                                 <div>
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                                        alt="Workflow"
-                                    />
+                                    {square_logo ? (
+                                            <Image
+                                                layout="fill"
+                                                objectFit="contain"
+                                                imageDetails={square_logo}
+                                                alt={newsroomName}
+                                                className="h-8 w-auto"
+                                            />
+                                    ) : (
+                                        <img
+                                            className="h-8 w-auto"
+                                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                                            alt="Workflow"
+                                        />
+                                    )}
                                 </div>
                                 <div className="-mr-2">
                                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -383,29 +393,15 @@ export default function Example() {
                         </div>
                         <div className="py-6 px-5">
                             <div className="grid grid-cols-2 gap-4">
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Pricing
-                                </a>
-
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Docs
-                                </a>
-
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Company
-                                </a>
-
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Resources
-                                </a>
-
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Blog
-                                </a>
-
-                                <a href="#" className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700">
-                                    Contact Sales
-                                </a>
+                                {resources.map((item) => (
+                                        <a
+                                            key={item.name}
+                                            href={item.href}
+                                            className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
+                                        >
+                                            {item.name}
+                                        </a>
+                                ))}
                             </div>
                         </div>
                     </div>
