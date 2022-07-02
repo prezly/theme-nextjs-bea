caf1cabb9177d582a85b1ac16a2516c4197874a4
Gijs Nelissen <gijs@prezly.com>
2022-07-02 15:48:54 +0200

    Update header blog roll

--- modules/Layout/Header/Header.tsx
+++ modules/Layout/Header/Header.tsx
@@ -75,19 +75,19 @@ const resources = [
 const blogPosts = [
     {
         id: 1,
-        name: 'Boost your conversion rate',
-        href: '#',
-        preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+        name: '10 tips for better product analytics',
+        href: '/10-tips-for-better-product-analytics',
+        preview: 'A list of tips & tricks I learned using segment.com, product analytics for a while',
         imageUrl:
-            'https://images.unsplash.com/photo-1558478551-1a378f63328e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2849&q=80',
+            'https://cdn.uc.assets.prezly.com/7c269d3a-20e2-40be-8cd9-4f3496c236ff/-/resize/3000/-/format/webp/',
     },
     {
         id: 2,
-        name: 'How to use search engine optimization to drive traffic to your site',
-        href: '#',
-        preview: 'Eget ullamcorper ac ut vulputate fames nec mattis pellentesque elementum. Viverra tempor id mus.',
+        name: 'Can I get rid of my phone?',
+        href: '/can-i-get-rid-of-my-phone',
+        preview: 'Because I learned the hard way that my phone usage was messing with my mood I\'ve been trying to limit my phone usage with mixed success.',
         imageUrl:
-            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2300&q=80',
+            'https://cdn.uc.assets.prezly.com/01138001-5d5f-4b3d-bd78-a68852d6769c/-/resize/3000/-/format/webp/',
     },
 ]
 
