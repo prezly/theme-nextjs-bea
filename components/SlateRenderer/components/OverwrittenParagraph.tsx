a72c1d288acb522d3c63d026ee6c1418cd69675a
Gijs Nelissen <gijs@prezly.com>
2022-07-05 09:01:17 +0200

    Add support for info boxes

--- components/SlateRenderer/components/OverwrittenParagraph.tsx
+++ components/SlateRenderer/components/OverwrittenParagraph.tsx
@@ -11,6 +11,18 @@ interface Props {
     children?: ReactNode;
 }
 
+const isInfoBox = (node: ParagraphNode) => {
+    // @ts-ignore
+    if (node && node.children[0].text) {
+        // @ts-ignore
+        const text = node.children[0].text as string;
+        if (text.substring(0, 1) === 'ℹ' || text.substring(0,1) === '💡') {
+            return true;
+        }
+    }
+    return false;
+}
+
 const isGitHubSnippet = (node: ParagraphNode) => {
     // @ts-ignore
     if (node && node.children[0].text) {
@@ -27,6 +39,22 @@ export function OverwrittenParagraph({ node, children }: Props) {
     if (isGitHubSnippet(node)) {
         // @ts-ignore
         return <GithubSnippet src={node.children[0].text as string} />;
+    }
+
+    if (isInfoBox(node)) {
+        return (
+            <div className="px-4 py-2 text-white bg-slate-700">
+                <p
+                    className={classNames(styles.paragraph, {
+                        [styles.alignLeft]: node.align === Alignment.LEFT,
+                        [styles.alignCenter]: node.align === Alignment.CENTER,
+                        [styles.alignRight]: node.align === Alignment.RIGHT,
+                    })}
+                >
+                    {children}
+                </p>
+            </div>
+        );
     }
 
     return (
