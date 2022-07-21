4956e769c62838785b7ae19ac698b691008f48cc
Oleg Semyonov <oleg@prezly.com>
2022-07-21 12:02:03 +0400

    Fix theme colors not being applied properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
             colors: {
                 transparent: 'transparent',
                 current: 'currentColor',
+                primary: 'var(--prezly-accent-color)',
+                'primary-tint': 'var(--prezly-accent-color-tint)',
+                'primary-shade': 'var(--prezly-accent-color-shade)',
             },
             fontFamily: {
                 sans: ['Satoshi', ...defaultTheme.fontFamily.sans],
