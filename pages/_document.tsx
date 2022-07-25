3faf80f7ecfec4949411a61283f48e8750d59ab0
Oleg Semyonov <oleg@prezly.com>
2022-07-25 14:18:02 +0400

    Use Document class from the Theme Kit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,31 +1,3 @@
-import type { PageProps } from '@prezly/theme-kit-nextjs';
-import { DEFAULT_LOCALE, getLocaleDirection, LocaleObject } from '@prezly/theme-kit-nextjs';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { PrezlyThemeDocument } from '@prezly/theme-kit-nextjs';
 
-class MyDocument extends Document {
-    render() {
-        const {
-            newsroomContextProps: { localeCode = DEFAULT_LOCALE },
-            // eslint-disable-next-line no-underscore-dangle
-        } = this.props.__NEXT_DATA__.props.pageProps as PageProps;
-
-        const locale = LocaleObject.fromAnyCode(localeCode);
-        // TODO: The direction can be pulled from the Language object
-        const direction = getLocaleDirection(locale);
-
-        return (
-            <Html lang={locale.toHyphenCode()} dir={direction}>
-                <Head>
-                    <meta name="og:locale" content={locale.toHyphenCode()} />
-                    {/* TODO: Add alternate locales */}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
-}
-
-export default MyDocument;
+export default PrezlyThemeDocument;
