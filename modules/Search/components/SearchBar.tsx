1f68a73707c37178c16d90e5e5811e5aff7b580d
Francis Gyimah <fgyimah883@gmail.com>
2022-07-26 08:17:45 +0000

    Added filter toggle buttons for mobile view

--- modules/Search/components/SearchBar.tsx
+++ modules/Search/components/SearchBar.tsx
@@ -1,6 +1,11 @@
+import { IconMenu } from '@prezly/icons';
 import translations from '@prezly/themes-intl-messages';
+import { Button } from '@prezly/themes-ui-components';
 import classNames from 'classnames';
+import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+
+import { useDevice } from '@/hooks/useDevice';
 
 import { AVAILABLE_FACET_ATTRIBUTES } from '../utils';
 
@@ -10,13 +15,29 @@ import SearchInput from './SearchInput';
 import styles from './SearchBar.module.scss';
 
 function Sidebar() {
+    const [isShown, setIsShown] = useState(false);
+    const { isMobile } = useDevice();
+
+    function toggleFacets() {
+        return setIsShown((s) => !s);
+    }
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <div className={styles.header}>
                     <SearchInput />
                 </div>
-                <div className={classNames(styles.facets)}>
+                {isMobile && (
+                    <Button
+                        variation="navigation"
+                        icon={IconMenu}
+                        onClick={toggleFacets}
+                        className={styles.toggleFacets}
+                    >
+                        <FormattedMessage {...translations.search.filters} />
+                    </Button>
+                )}
+                <div className={classNames(styles.facets, { [styles.facetsOpen]: isShown })}>
                     <p className={styles.filters}>
                         <FormattedMessage {...translations.search.filters} />
                     </p>
