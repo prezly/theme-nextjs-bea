64e3b51e7b1da863bd162532dbc9be5e88978f42
Francis Gyimah <fgyimah883@gmail.com>
2022-07-26 07:51:28 +0000

    Updated search bar design for tablet and mobile views

--- modules/Search/components/SearchBar.tsx
+++ modules/Search/components/SearchBar.tsx
@@ -1,9 +1,6 @@
-import { IconFilter } from '@prezly/icons';
 import translations from '@prezly/themes-intl-messages';
-import { Button } from '@prezly/themes-ui-components';
 import classNames from 'classnames';
-import { useState } from 'react';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import { AVAILABLE_FACET_ATTRIBUTES } from '../utils';
 
@@ -13,27 +10,13 @@ import SearchInput from './SearchInput';
 import styles from './SearchBar.module.scss';
 
 function Sidebar() {
-    const { formatMessage } = useIntl();
-    const [isShown, setIsShown] = useState(false);
-
-    function toggleFacets() {
-        return setIsShown((s) => !s);
-    }
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <div className={styles.header}>
                     <SearchInput />
-                    <Button
-                        variation="secondary"
-                        icon={IconFilter}
-                        title={formatMessage(translations.actions.toggleFilters)}
-                        onClick={toggleFacets}
-                        className={styles.button}
-                    />
                 </div>
-                <div className={classNames(styles.facets, { [styles.facetsOpen]: isShown })}>
+                <div className={classNames(styles.facets)}>
                     <p className={styles.filters}>
                         <FormattedMessage {...translations.search.filters} />
                     </p>
