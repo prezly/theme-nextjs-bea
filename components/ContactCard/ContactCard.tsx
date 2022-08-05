3e01c1d0d4ca725da1f248d66ad50ded6417023a
Gichohi-Simon <njogugichohi56@gmail.com>
2022-08-05 16:26:37 +0300

    made design changes to the contact card

--- components/ContactCard/ContactCard.tsx
+++ components/ContactCard/ContactCard.tsx
@@ -15,13 +15,11 @@ interface Props {
     isCompact?: boolean;
     renderAvatar: ({ className }: { className: string }) => ReactNode;
 }
-
 function ContactCard({ className, contact, isCompact = false, renderAvatar }: Props) {
     const device = useDevice();
     const { name, description, company, email, phone, mobile, website } = contact;
     const { facebook, twitter } = getSocialHandles(contact);
     const subtitle = description && company ? `${description}, ${company}` : description;
-
     return (
         <div
             id={`contact-${contact.uuid}`}
@@ -29,52 +27,59 @@ function ContactCard({ className, contact, isCompact = false, renderAvatar }: Pr
                 [styles.compact]: isCompact || device.isMobile,
             })}
         >
-            {renderAvatar({ className: styles.avatar })}
             <div className={styles.content}>
-                <h4 className={styles.name}>{name}</h4>
-                {subtitle && <h5 className={styles.position}>{subtitle}</h5>}
-
+                <div className={styles.contentTitle}>
+                    {renderAvatar({ className: styles.avatar })}
+                    <div>
+                        <h4 className={styles.name}>{name}</h4>
+                        {subtitle && <h5 className={styles.position}>{subtitle}</h5>}
+                    </div>
+                </div>
                 <div className={styles.links}>
-                    {email && (
-                        <a href={`mailto:${email}`} className={styles.link}>
-                            <IconEmail width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{email}</span>
-                        </a>
-                    )}
+                    <div className={styles.contactSection1}>
+                        {email && (
+                            <a href={`mailto:${email}`} className={styles.link}>
+                                <IconEmail width={16} height={16} className={styles.icon} />
+                                <span className={styles.linkText}>{email}</span>
+                            </a>
+                        )}
+                        {phone && (
+                            <a href={`tel:${phone}`} className={styles.link}>
+                                <IconPhone width={16} height={16} className={styles.icon} />
+                                <span className={styles.linkText}>{phone}</span>
+                            </a>
+                        )}
+                        {mobile && (
+                            <a href={`tel:${mobile}`} className={styles.link}>
+                                <IconPhone width={16} height={16} className={styles.icon} />
+                                <span className={styles.linkText}>{mobile}</span>
+                            </a>
+                        )}
+                    </div>
+
                     {website && (
                         <a href={website} className={styles.link}>
                             <IconGlobe width={16} height={16} className={styles.icon} />
                             <span className={styles.linkText}>{website}</span>
                         </a>
                     )}
-                    {mobile && (
-                        <a href={`tel:${mobile}`} className={styles.link}>
-                            <IconPhone width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{mobile}</span>
-                        </a>
-                    )}
-                    {phone && (
-                        <a href={`tel:${phone}`} className={styles.link}>
-                            <IconPhone width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{phone}</span>
-                        </a>
-                    )}
-                    {twitter && (
-                        <a href={`https://twitter.com/${twitter}`} className={styles.link}>
-                            <IconTwitter width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{`@${twitter}`}</span>
-                        </a>
-                    )}
-                    {facebook && (
-                        <a href={`https://facebook.com/${facebook}`} className={styles.link}>
-                            <IconFacebook width={16} height={16} className={styles.icon} />
-                            <span className={styles.linkText}>{facebook}</span>
-                        </a>
-                    )}
+                    <div className={styles.socials}>
+                        {facebook && (
+                            <a href={`https://facebook.com/${facebook}`} className={styles.link}>
+                                <IconFacebook width={16} height={16} className={styles.icon} />
+                                <span className={styles.linkText}>{facebook}</span>
+                            </a>
+                        )}
+                        {twitter && (
+                            <a href={`https://twitter.com/${twitter}`} className={styles.link}>
+                                <IconTwitter width={16} height={16} className={styles.icon} />
+                                <span className={styles.linkText}>{`@${twitter}`}</span>
+                            </a>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
 export default ContactCard;
