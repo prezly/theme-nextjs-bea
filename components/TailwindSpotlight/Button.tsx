aba98761d17d5787e7c2574f8e5129e590b5d726
Gijs Nelissen <gijs@prezly.com>
2022-09-09 23:17:05 +0200

    Add newsletter block

---
+++ components/TailwindSpotlight/Button.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import Link from 'next/link';
+
+const primaryVariantClassnames =
+    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70';
+const secondaryVariantClassnames =
+    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70';
+
+interface ButtonProps {
+    variant: string;
+    className?: string;
+    href: string;
+}
+
+export function Button({ variant = 'primary', className, href, ...props }: ButtonProps) {
+    const classNameWithVariant = classNames(
+        'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+        className,
+        {
+            [primaryVariantClassnames]: variant === 'primary',
+            [secondaryVariantClassnames]: variant === 'secondary',
+        },
+    );
+
+    return href ? (
+        <Link href={href} className={classNameWithVariant} {...props} />
+    ) : (
+        <button className={classNameWithVariant} {...props} />
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,13 @@ import Link from 'next/link';
 import type { FunctionComponent } from 'react';
 import { useIntl } from 'react-intl';
 
+import { Button } from '@/components/TailwindSpotlight/Button';
+import { Card } from '@/components/TailwindSpotlight/Card';
 import { Container } from '@/components/TailwindSpotlight/Container';
 import { GitHubIcon, LinkedInIcon, TwitterIcon } from '@/components/TailwindSpotlight/SocialIcons';
 import Layout from '@/modules/Layout';
 import { importMessages, isTrackingEnabled, loadFeaturedStories } from '@/utils';
+import { formatDate } from '@/utils/formatDate';
 import image2 from 'public/images/gijs-ball.jpeg';
 import image4 from 'public/images/gijs-desk.jpeg';
 import image3 from 'public/images/gijs-outlook.jpeg';
@@ -34,11 +37,79 @@ interface SocialLinkProps {
     ariaLabel: string;
 }
 
+interface ArticleProps {
+    article: StoryWithImage;
+}
+
+function MailIcon({ className }: IconTypeProps) {
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            aria-hidden="true"
+            className={className}
+        >
+            <path
+                d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+                className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
+            />
+            <path
+                d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
+                className="stroke-zinc-400 dark:stroke-zinc-500"
+            />
+        </svg>
+    );
+}
+
 function SocialLink({ icon: Icon, href, ariaLabel }: SocialLinkProps) {
     return (
         <Link className="group -m-1 p-1" href={href} aria-label={ariaLabel}>
             <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
         </Link>
+    );
+}
+
+function Newsletter() {
+    return (
+        <form
+            action="/thank-you"
+            className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+        >
+            <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+                <MailIcon className="h-6 w-6 flex-none" />
+                <span className="ml-3">Stay up to date</span>
+            </h2>
+            <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+                Get notified when I publish something new, and unsubscribe at any time.
+            </p>
+            <div className="mt-6 flex">
+                <input
+                    type="email"
+                    placeholder="Email address"
+                    aria-label="Email address"
+                    required
+                    className="flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+                />
+                <Button type="submit" className="ml-4 flex-none">
+                    Join
+                </Button>
+            </div>
+        </form>
+    );
+}
+
+function Article({ article: story }: ArticleProps) {
+    const dateAsString = story.published_at ?? '';
+    return (
+        <Card>
+            <Card.Title href={story.slug}>{story.title}</Card.Title>
+            <Card.Eyebrow decorate>{formatDate(dateAsString)}</Card.Eyebrow>
+            <Card.Description>{story.summary}</Card.Description>
+            <Card.Cta>Read article</Card.Cta>
+        </Card>
     );
 }
 
@@ -68,7 +139,7 @@ function Photos() {
     );
 }
 
-const IndexPage: FunctionComponent<Props> = () => {
+const IndexPage: FunctionComponent<Props> = ({ stories }) => {
     const companyInformation = useCompanyInformation();
     const { formatMessage } = useIntl();
 
@@ -113,13 +184,12 @@ const IndexPage: FunctionComponent<Props> = () => {
             <Container className="mt-24 md:mt-28">
                 <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
                     <div className="flex flex-col gap-16">
-                        test
-                        {/* {articles.map((article) => ( */}
-                        {/*    <Article key={article.slug} article={article} /> */}
-                        {/* ))} */}
+                        {stories.slice(0, 4).map((story) => (
+                            <Article key={story.slug} article={story} />
+                        ))}
                     </div>
                     <div className="space-y-10 lg:pl-16 xl:pl-24">
-                        {/* <Newsletter /> */}
+                        <Newsletter />
                         {/* <Resume /> */}
                     </div>
                 </div>
@@ -135,7 +205,6 @@ export const getStaticProps = getHomepageStaticProps<BasePageProps, StoryWithIma
         translations: await importMessages(newsroomContextProps.localeCode),
         featuredStories: await loadFeaturedStories(context),
     }),
-    { extraStoryFields: ['thumbnail_image'] },
 );
 
 export default IndexPage;
