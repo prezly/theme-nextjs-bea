917b48b899d2af0d93ddb4b519072b213b275b35
Gijs Nelissen <gijs@prezly.com>
2022-09-15 00:15:25 +0200

    Support children in Links and Buttons

--- components/TailwindSpotlight/Button.tsx
+++ components/TailwindSpotlight/Button.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import Link from 'next/link';
+import type {ReactNode} from 'react';
 
 const primaryVariantClassnames =
     'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70';
@@ -10,10 +11,11 @@ interface ButtonProps {
     variant?: string;
     className?: string;
     href?: string;
+    children?: ReactNode;
     buttonType?: 'button' | 'submit' | 'reset';
 }
 
-export function Button({ variant = 'primary', className, href, buttonType }: ButtonProps) {
+export function Button({variant = 'primary', className, href, children, buttonType}: ButtonProps) {
     const classNameWithVariant = classNames(
         'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
         className,
@@ -24,8 +26,12 @@ export function Button({ variant = 'primary', className, href, buttonType }: But
     );
 
     return href ? (
-        <Link href={href} className={classNameWithVariant} />
+        <Link href={href} className={classNameWithVariant}>
+            {children}
+        </Link>
     ) : (
-        <button className={classNameWithVariant} type={buttonType} />
+        <button className={classNameWithVariant} type={buttonType}>
+            {children}
+        </button>
     );
 }
