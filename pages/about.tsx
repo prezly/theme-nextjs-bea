e8f1b0f8d376dc98fe0a4828092ccdeb78ec5f89
Gijs Nelissen <gijs@prezly.com>
2022-09-15 00:12:57 +0200

    Reduce build errors for about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,21 +14,6 @@ import type { BasePageProps, StoryWithImage } from 'types';
 
 type Props = BasePageProps & HomePageProps<StoryWithImage>;
 
-interface IconTypeProps {
-    className: string;
-}
-
-function MailIcon({ className }: IconTypeProps) {
-    return (
-        <svg viewBox="0 0 24 24" aria-hidden="true" className={className}>
-            <path
-                fillRule="evenodd"
-                d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-            />
-        </svg>
-    );
-}
-
 const AboutPage: FunctionComponent<Props> = ({}) => (
     <Layout title="About Me">
         <Container className="mt-16 sm:mt-32">
