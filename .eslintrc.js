a6f2441a5ed5a13eba7c66316e17b6a2fa7f5b0d
Oleg Semyonov <oleg@prezly.com>
2023-04-20 16:23:41 +0400

    Upgrade ESLint config with Next 13 support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['@prezly', '@prezly/eslint-config/react', 'next/core-web-vitals'],
+    extends: ['@prezly', '@prezly/eslint-config/react', '@prezly/eslint-config/nextjs'],
     rules: {
         // TODO: NextJS Dynamic import doesn't work too well with named exports.
         // Gotta figure out how to make them work together.
@@ -22,68 +22,5 @@ module.exports = {
                 exceptions: ['FormattedMessage'],
             },
         ],
-
-        // Ported from `@prezly/eslint-config/next`
-        'import/order': [
-            'error',
-            {
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true,
-                },
-                groups: [
-                    ['builtin', 'external'],
-                    'internal',
-                    'parent',
-                    'sibling',
-                    'index',
-                    // This is used for assets and styles, since `import/order` doesn't allow custom group names
-                    'unknown',
-                ],
-                'newlines-between': 'always',
-                pathGroups: [
-                    {
-                        pattern: '*.{svg|png|jpg|jpeg}',
-                        patternOptions: { matchBase: true },
-                        group: 'unknown',
-                        position: 'before',
-                    },
-                    {
-                        pattern: '@/public/images/**',
-                        group: 'unknown',
-                        position: 'before',
-                    },
-                    {
-                        pattern: '*.scss',
-                        patternOptions: { matchBase: true },
-                        group: 'unknown',
-                        position: 'after',
-                    },
-                    // For some reason, `*.{scss|css}` makes the config not working as expected
-                    {
-                        pattern: '*.css',
-                        patternOptions: { matchBase: true },
-                        group: 'unknown',
-                        position: 'after',
-                    },
-                    {
-                        pattern: '@/**',
-                        group: 'internal',
-                    },
-                ],
-            },
-        ],
     },
-    overrides: [
-        {
-            files: ['pages/**'],
-            rules: {
-                // Next.js needs default exports for pages and API points
-                'import/no-default-export': 'off',
-                // Next.js `getServerSideProps` has a quite complex type, so it's safer to define it as a const
-                // and have the `GetServerSideProps<P>` type applied to it for better type safety
-                'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
-            },
-        },
-    ],
 };
