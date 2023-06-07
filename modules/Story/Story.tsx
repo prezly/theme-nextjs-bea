61656bf57e5ab459ffc5a194a51eeaa5205d8a58
kudlajz <kudlajzzz@gmail.com>
2023-06-07 10:44:54 +0200

    Implement header renderer in story, rename SlateRenderer to ContentRenderer

--- modules/Story/Story.tsx
+++ modules/Story/Story.tsx
@@ -1,21 +1,20 @@
 import { useAnalyticsContext } from '@prezly/analytics-nextjs';
 import type { ExtendedStory } from '@prezly/sdk';
-import { FormatVersion, isEmbargoStory } from '@prezly/theme-kit-core';
+import { isEmbargoStory } from '@prezly/theme-kit-core';
 import { StorySeo } from '@prezly/theme-kit-nextjs';
-import Image from '@prezly/uploadcare-image';
-import classNames from 'classnames';
 import dynamic from 'next/dynamic';
 
 import { StoryPublicationDate } from '@/components';
 import { useThemeSettings } from '@/hooks';
-import { getStoryImageSizes } from '@/utils';
 
 import Layout from '../Layout';
+
+import { HeaderRenderer } from './StoryRenderer';
 
 import styles from './Story.module.scss';
 
 const CategoriesList = dynamic(() => import('@/components/CategoriesList'));
-const SlateRenderer = dynamic(() => import('@/components/SlateRenderer'));
+const ContentRenderer = dynamic(() => import('@/components/ContentRenderer'));
 const StoryLinks = dynamic(() => import('@/components/StoryLinks'));
 const Embargo = dynamic(() => import('./Embargo'));
 
@@ -31,47 +30,25 @@ function Story({ story }: Props) {
         return null;
     }
 
-    const { title, subtitle, content, format_version, categories, links } = story;
-    const headerImage = story.header_image ? JSON.parse(story.header_image) : null;
-    const hasHeaderImage = Boolean(headerImage);
+    const { categories, links } = story;
     const hasCategories = categories.length > 0;
+    const nodes = JSON.parse(story.content);
 
     return (
         <Layout>
             <StorySeo story={story} noindex={!isAnalyticsEnabled} />
             <article className={styles.story}>
-                <div
-                    className={classNames(styles.container, {
-                        [styles.withImage]: hasHeaderImage,
-                    })}
-                >
+                <div className={styles.container}>
+                    {isEmbargoStory(story) && <Embargo story={story} />}
                     {hasCategories && <CategoriesList categories={categories} showAllCategories />}
-                    <h1 className={styles.title}>{title}</h1>
-                    <p className={styles.subtitle}>{subtitle}</p>
+                    <HeaderRenderer nodes={nodes} />
                     {showDate && (
                         <p className={styles.date}>
                             <StoryPublicationDate story={story} />
                         </p>
                     )}
                     <StoryLinks url={links.short || links.newsroom_view} />
-                    {headerImage && (
-                        <Image
-                            alt=""
-                            className={styles.mainImage}
-                            objectFit="cover"
-                            layout="fill"
-                            imageDetails={headerImage}
-                            sizes={getStoryImageSizes()}
-                        />
-                    )}
-                    {isEmbargoStory(story) && <Embargo story={story} />}
-                    {format_version === FormatVersion.HTML && (
-                        // eslint-disable-next-line react/no-danger
-                        <div dangerouslySetInnerHTML={{ __html: content }} />
-                    )}
-                    {format_version === FormatVersion.SLATEJS && (
-                        <SlateRenderer nodes={JSON.parse(content as string)} />
-                    )}
+                    <ContentRenderer nodes={nodes} />
                 </div>
             </article>
         </Layout>
---
+++ modules/Story/StoryRenderer/index.ts
@@ -0,0 +1,1 @@
+export { HeaderRenderer } from './HeaderRenderer';
