09269f9f1d894926031fb2f101ca9f1c405fa670
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-25 12:23:18 +0300

    Revert accidental change

--- next.config.js
+++ next.config.js
@@ -49,7 +49,7 @@ const sentryWebpackPluginOptions = {
     // Additional config options for the Sentry Webpack plugin. Keep in mind that
     // the following options are set automatically, and overriding them is not
     // recommended:
-    //   release, published, org, project, authToken, configFile, stripPrefix,
+    //   release, url, org, project, authToken, configFile, stripPrefix,
     //   urlPrefix, include, ignore
     // silent: true, // Suppresses all logs
     // For all available options, see:
