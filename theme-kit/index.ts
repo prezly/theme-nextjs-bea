cbb50c2c5d8bda5837d26877774f900621a3a020
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-25 12:07:35 +0300

    Move app router configuration and middleware implementation to the `theme-kit` module
    (will be moved to the `theme-kit` package later)

--- theme-kit/index.ts
+++ theme-kit/index.ts
@@ -2,3 +2,4 @@ export { api } from './api';
 export { env, bootHttpEnv } from './env';
 export { createRouter, route } from './router';
 export { locale } from './locale';
+export { middleware } from './middleware';
