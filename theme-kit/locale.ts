2c0eda7a10e7d8c55c5bfacd29b83ff7965d2121
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-25 14:53:50 +0300

    Make sure `locale()` always returns a string (or fails)

--- theme-kit/locale.ts
+++ theme-kit/locale.ts
@@ -1,7 +1,16 @@
 import { headers } from 'next/headers';
 
 export function locale() {
-    return headers().get(locale.HEADER);
+    const code = headers().get(locale.HEADER);
+
+    if (!code) {
+        console.error(
+            'Locale header is not set. Please check if the middleware is configured properly.',
+        );
+        return 'en';
+    }
+
+    return code;
 }
 
 export namespace locale {
