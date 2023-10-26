d43840e6e9b6f58c96c9ef4a7cd466ef593cc827
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-10-26 14:17:18 +0300

    Apparently, we cannot use a shared variable for middleware `config`
    See https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
    
    > *Good to know*: The `matcher` values need to be constants so they can be statically
    > analyzed at build-time. Dynamic values such as variables will be ignored.

--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,15 @@ import { middleware } from './theme-kit';
 
 export { middleware };
 
-export const { config } = middleware;
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    ],
+};
