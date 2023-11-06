727c3eb6148a3c738e2a4c6fd9bfb2d05fcbd111
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-06 11:43:01 +0200

    Align `ButtonLink` props definition with the underlying `Link` component

--- ui/Button/ButtonLink.tsx
+++ ui/Button/ButtonLink.tsx
@@ -1,6 +1,4 @@
 import classNames from 'classnames';
-import type { PropsWithChildren } from 'react';
-import { forwardRef } from 'react';
 
 import { Link } from '@/components/Link';
 
@@ -9,30 +7,18 @@ import type { BaseProps } from './types';
 
 import styles from './Button.module.scss';
 
-export interface LinkProps extends BaseProps, Link.Props {
-    forceRefresh?: boolean;
-}
-
-export const ButtonLink = forwardRef<
-    HTMLAnchorElement,
-    Omit<PropsWithChildren<LinkProps>, 'onResize' | 'onResizeCapture'>
->(
-    (
-        {
-            children,
-            className,
-            href,
-            icon,
-            iconPlacement = 'left',
-            variation,
-            forceRefresh,
-            ...attributes
-        },
-        ref,
-    ) => (
+export function ButtonLink({
+    children,
+    className,
+    href,
+    icon,
+    iconPlacement = 'left',
+    variation,
+    ...attributes
+}: ButtonLink.Props) {
+    return (
         <Link
             href={href}
-            forwardRef={ref}
             className={classNames(styles.button, className, {
                 [styles.primary]: variation === 'primary',
                 [styles.secondary]: variation === 'secondary',
@@ -46,7 +32,9 @@ export const ButtonLink = forwardRef<
             {children && <span className={styles.label}>{children}</span>}
             {iconPlacement === 'right' && <Icon icon={icon} placement="right" />}
         </Link>
-    ),
-);
+    );
+}
 
-ButtonLink.displayName = 'ButtonLink';
+export namespace ButtonLink {
+    export type Props = BaseProps & Omit<Link.Props, 'onResize' | 'onResizeCapture'>;
+}
