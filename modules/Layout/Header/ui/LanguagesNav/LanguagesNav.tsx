393ebb00c60b3e02b65a5c1747823f11a937a6da
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-07 15:28:49 +0200

    [DEV-11945] Make Languages dropdown automatically pull links from the `<head>` metadata

--- modules/Layout/Header/ui/LanguagesNav/LanguagesNav.tsx
+++ modules/Layout/Header/ui/LanguagesNav/LanguagesNav.tsx
@@ -1,19 +1,67 @@
 'use client';
 
 import type { Culture } from '@prezly/sdk';
-import type { Locale } from '@prezly/theme-kit-intl/build/cjs';
+import { Locale } from '@prezly/theme-kit-intl';
+import { isNotUndefined } from '@technically/is-not-undefined';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 import type { Props as LanguageDropdownProps } from './LanguagesDropdown';
 import { LanguagesDropdown } from './LanguagesDropdown';
+import type { LanguageDisplayProps } from './types';
 
 interface Props extends Omit<LanguageDropdownProps, 'selected' | 'options'> {
     localeCode: Locale.Code;
     languages: Record<Locale.Code, Culture['native_name']>;
 }
 
-export function LanguagesNav({ languages, ...attributes }: Props) {
-    const options: never[] = []; // FIXME
-    const selected = options[0]; // FIXME
+interface State {
+    options: LanguageDisplayProps[];
+    selected: LanguageDisplayProps | undefined;
+}
+
+export function LanguagesNav({ localeCode, languages, ...attributes }: Props) {
+    const [mounted, setMounted] = useState(false);
+    const [{ options, selected }, setState] = useState<State>({
+        options: [],
+        selected: {
+            code: localeCode,
+            title: languages[localeCode],
+            href: '',
+        },
+    });
+    const pathname = usePathname();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        const languageOptions: LanguageDisplayProps[] = Array.from(
+            document.head.querySelectorAll('link[rel="alternate"]'),
+        )
+            .filter((link) => link instanceof HTMLLinkElement)
+            .map((link) => {
+                if (link instanceof HTMLLinkElement && Locale.isValid(link.hreflang)) {
+                    const { code } = Locale.from(link.hreflang);
+                    const { href } = link;
+                    const title = languages[code];
+
+                    if (title) {
+                        return { code, href, title };
+                    }
+                }
+                return undefined;
+            })
+            .filter(isNotUndefined);
+
+        const selectedLanguage = options.find((option) => option.code === localeCode);
+
+        setState({
+            options: languageOptions,
+            selected: selectedLanguage,
+        });
+    }, [mounted, pathname]);
 
     if (!selected) {
         return null;
