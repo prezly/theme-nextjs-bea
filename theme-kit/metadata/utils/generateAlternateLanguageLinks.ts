e2ce5ea847876c931845945045ac750bd0103f63
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-08 00:24:29 +0200

    Revert alternate links titles attribute generation

--- theme-kit/metadata/utils/generateAlternateLanguageLinks.ts
+++ theme-kit/metadata/utils/generateAlternateLanguageLinks.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import type { NewsroomLanguageSettings } from '@prezly/sdk';
 import { getAlternateLanguageLinks } from '@prezly/theme-kit-core';
-import { Locale } from '@prezly/theme-kit-intl';
+import type { Locale } from '@prezly/theme-kit-intl';
 import type { Metadata } from 'next';
 
 import { api } from '@/theme-kit';
@@ -16,23 +15,7 @@ export async function generateAlternateLanguageLinks(
     const { contentDelivery } = api();
     const languages = await contentDelivery.languages();
 
-    const links = getAlternateLanguageLinksWithTitles(languages, generateUrl);
-
-    return Object.fromEntries(
-        links.map((link) => [link.hrefLang, [{ url: link.href, title: link.title }]]),
-    );
-}
-
-function getAlternateLanguageLinksWithTitles<
-    Language extends Pick<NewsroomLanguageSettings, 'code' | 'is_default' | 'locale'>,
->(languages: Language[], generateUrl: (locale: Locale) => string | undefined) {
     const links = getAlternateLanguageLinks(languages, generateUrl);
 
-    return links.map(({ href, hrefLang }) => {
-        const language = languages.find((lang) => Locale.from(lang.code).isoCode === hrefLang);
-        if (language) {
-            return { href, hrefLang, title: language.locale.native_name };
-        }
-        return { href, hrefLang };
-    });
+    return Object.fromEntries(links.map((link) => [link.hrefLang, link.href]));
 }
