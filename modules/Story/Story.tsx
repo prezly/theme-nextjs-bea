3170269dcae2b6ad922f1e9198a8cb8f75b8ae3f
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-08 16:31:08 +0200

    [DEV-11930] Deduplicate categories fetching

--- modules/Story/Story.tsx
+++ modules/Story/Story.tsx
@@ -1,14 +1,12 @@
 import type { ExtendedStory } from '@prezly/sdk';
 import { Alignment } from '@prezly/story-content-format';
-import { getCategoryHasTranslation, getLocalizedCategoryData } from '@prezly/theme-kit-core';
-import { isNotUndefined } from '@technically/is-not-undefined';
 import classNames from 'classnames';
 
 import { CategoriesList } from '@/components/CategoriesList';
 import { ContentRenderer } from '@/components/ContentRenderer';
 import { StoryLinks } from '@/components/StoryLinks';
 import { themeSettings } from '@/theme/settings/server';
-import { locale, routing } from '@/theme-kit';
+import { displayedCategories } from '@/theme-kit';
 import { FormattedDate } from '@/theme-kit/intl/client';
 import { getHeaderAlignment } from '@/utils';
 
@@ -22,37 +20,21 @@ type Props = {
 };
 
 export async function Story({ story }: Props) {
-    const { code: localeCode } = locale();
-    const { generateUrl } = await routing();
     const settings = await themeSettings();
 
-    const { categories, links } = story;
-    const hasCategories = categories.length > 0;
+    const { links } = story;
     const nodes = JSON.parse(story.content);
 
     const headerAlignment = getHeaderAlignment(nodes);
 
-    // FIXME: Add a helper function to deduplicate this code everywhere
-    const displayedCategories = categories
-        .filter((category) => getCategoryHasTranslation(category, localeCode))
-        .map((category) => {
-            const { id } = category;
-            const { name, description, slug } = getLocalizedCategoryData(category, localeCode);
-            if (slug) {
-                const href = generateUrl('category', { slug, localeCode });
-
-                return { id, name, description, href };
-            }
-            return undefined;
-        })
-        .filter(isNotUndefined);
+    const categories = await displayedCategories(story.categories);
 
     return (
         <article className={styles.story}>
             <div className={styles.container}>
                 <Embargo story={story} />
-                {hasCategories && (
-                    <CategoriesList categories={displayedCategories} showAllCategories />
+                {categories.length > 0 && (
+                    <CategoriesList categories={categories} showAllCategories />
                 )}
                 <HeaderRenderer nodes={nodes} />
                 <div
