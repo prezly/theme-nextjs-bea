22759d8ff23bff3e5c25008b46953a36941683cf
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-09 03:41:25 +0200

    [DEV-11916] Drop old _page routes

--- _pages/api/fetch-stories.ts
+++
@@ -1,4 +0,0 @@
-import { fetchStories } from '@prezly/theme-kit-nextjs/server';
-import { wrapApiHandlerWithSentry } from '@sentry/nextjs';
-
-export default wrapApiHandlerWithSentry(fetchStories, '/api/fetch-stories');
