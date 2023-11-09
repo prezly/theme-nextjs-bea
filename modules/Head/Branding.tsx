a70677645612750f9748bda79761cbb4856906c4
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-09 03:57:37 +0200

    [DEV-11916] Use dynamic import for DynamicPreviewBranding

--- modules/Head/Branding.tsx
+++ modules/Head/Branding.tsx
@@ -1,10 +1,18 @@
 import { getNewsroomFaviconUrl } from '@prezly/theme-kit-core';
+import dynamic from 'next/dynamic';
 
 import { themeSettings } from '@/theme/settings/server';
 import { api } from '@/theme-kit';
 
 import { BrandingSettings } from './branding';
-import { DynamicPreviewBranding } from './branding/DynamicPreviewBranding';
+
+const DynamicPreviewBranding = dynamic(
+    async () => {
+        const component = await import('./branding/DynamicPreviewBranding');
+        return { default: component.DynamicPreviewBranding };
+    },
+    { ssr: false },
+);
 
 export async function Branding() {
     const { contentDelivery } = api();
