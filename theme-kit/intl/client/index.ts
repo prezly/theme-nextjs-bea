60566f08109d0198035e6a3c568c10bc4cc4eaf8
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-09 17:22:11 +0200

    [DEV-11917] Drop `IntlLink`
    It can be fully replaced with the generic route-aware `Link` component

--- theme-kit/intl/client/index.ts
+++ theme-kit/intl/client/index.ts
@@ -1,6 +1,5 @@
 export * from '../types';
 
 export { IntlContextProvider, useIntl } from './context';
-export { IntlLink } from './IntlLink';
 export { useLocaleSlug } from './useLocaleSlug';
 export { FormattedMessage, FormattedDate, FormattedTime } from './components';
