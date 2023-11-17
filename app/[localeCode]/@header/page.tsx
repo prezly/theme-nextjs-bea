ae3d01c67eef75f5ed236b33b29f39565d094eed
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-17 13:54:50 +0200

    Undo parallel slots

--- app/[localeCode]/@header/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Header } from '@/modules/Header';
-
-export default function HeaderSlot() {
-    return <Header />;
-}
--- app/[localeCode]/layout.tsx
+++ app/[localeCode]/layout.tsx
@@ -1,8 +1,12 @@
 import type { Locale } from '@prezly/theme-kit-nextjs';
 import type { ReactNode } from 'react';
 
+import { Boilerplate } from '@/modules/Boilerplate';
 import { CookieConsent } from '@/modules/CookieConsent';
+import { Footer } from '@/modules/Footer';
+import { Header } from '@/modules/Header';
 import { Notifications } from '@/modules/Notifications';
+import { SubscribeForm } from '@/modules/SubscribeForm';
 
 import styles from './layout.module.scss';
 
@@ -10,20 +14,20 @@ interface Props {
     params: {
         localeCode: Locale.Code;
     };
-    header: ReactNode;
-    main: ReactNode;
-    footer: ReactNode;
+    children: ReactNode;
 }
 
-export default async function MainLayout({ header, main, footer }: Props) {
+export default async function MainLayout({ children }: Props) {
     return (
         <>
             <Notifications />
             <CookieConsent />
             <div className={styles.layout}>
-                {header}
-                <main className={styles.content}>{main}</main>
-                {footer}
+                <Header />
+                <main className={styles.content}>{children}</main>
+                <SubscribeForm />
+                <Boilerplate />
+                <Footer />
             </div>
         </>
     );
