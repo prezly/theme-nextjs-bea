6a8979e471c84845b5224d6097633d4aac4ec4ff
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-27 21:22:16 +0200

    [DEV-12054] Simplify code by using `Category.translations()`

--- app/[localeCode]/category/[slug]/page.tsx
+++ app/[localeCode]/category/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import type { Category } from '@prezly/sdk';
+import { Category } from '@prezly/sdk';
 import type { Locale } from '@prezly/theme-kit-nextjs';
 import { DEFAULT_PAGE_SIZE } from '@prezly/theme-kit-nextjs';
 import type { Metadata } from 'next';
@@ -45,9 +45,9 @@ export default async function CategoryPage({ params }: Props) {
 async function BroadcastCategoryTranslations(props: { category: Category }) {
     const { generateUrl } = await routing();
 
-    const translations = Object.values(props.category.i18n).map(({ slug, locale }) => ({
-        code: locale.code,
-        href: slug ? generateUrl('category', { slug, localeCode: locale.code }) : undefined,
+    const translations = Category.translations(props.category).map(({ slug, locale }) => ({
+        code: locale,
+        href: slug ? generateUrl('category', { slug, localeCode: locale }) : undefined,
     }));
 
     return <BroadcastTranslations translations={translations} />;
