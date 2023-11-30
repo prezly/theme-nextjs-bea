7f02ecf38c77a3f3811f97554e610527db992059
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2023-11-30 02:32:50 +0200

    [DEV-12039] Only expose white-listed env variables to the request context
    To avoid leaking sensitive values

--- remix/environment.ts
+++ remix/environment.ts
@@ -1,21 +1,19 @@
 import { z } from 'zod';
 
-const Schema = z
-    .object({
-        NEXT_PUBLIC_BASE_URL: z.string().optional(),
+const Schema = z.object({
+    NEXT_PUBLIC_BASE_URL: z.string().optional(),
 
-        PREZLY_ACCESS_TOKEN: z.string(),
-        PREZLY_NEWSROOM_UUID: z.string(),
-        PREZLY_THEME_UUID: z.string().optional(),
-        PREZLY_API_BASEURL: z.string().optional(),
+    PREZLY_ACCESS_TOKEN: z.string(),
+    PREZLY_NEWSROOM_UUID: z.string(),
+    PREZLY_THEME_UUID: z.string().optional(),
+    PREZLY_API_BASEURL: z.string().optional(),
 
-        ALGOLIA_API_KEY: z.string().optional(),
-        ALGOLIA_APP_ID: z.string().optional(),
-        ALGOLIA_INDEX: z.string().optional(),
+    ALGOLIA_API_KEY: z.string().optional(),
+    ALGOLIA_APP_ID: z.string().optional(),
+    ALGOLIA_INDEX: z.string().optional(),
 
-        PREZLY_MODE: z.string().optional(),
-    })
-    .catchall(z.string());
+    PREZLY_MODE: z.string().optional(),
+});
 
 export type Environment = z.infer<typeof Schema>;
 
