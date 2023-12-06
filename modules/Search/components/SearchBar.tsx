d417a7e07e0db6e95f40b2fde9d8b9cc4a2649d8
Oleg Semyonov <oleg@prezly.com>
2023-12-06 17:38:54 +0400

    Apply a hack to show more than 10 facets in the search dropdown

--- modules/Search/components/SearchBar.tsx
+++ modules/Search/components/SearchBar.tsx
@@ -42,7 +42,13 @@ function Sidebar() {
                         <FormattedMessage {...translations.search.filters} />
                     </p>
                     {AVAILABLE_FACET_ATTRIBUTES.map((attribute) => (
-                        <Facet key={attribute} attribute={attribute} />
+                        <Facet
+                            key={attribute}
+                            attribute={attribute}
+                            // This is a hack to make Algolia return more than 10 facets by default. We need to upgrade to v7 to allow finer control over this.
+                            showMore
+                            showMoreLimit={50}
+                        />
                     ))}
                 </div>
             </div>
