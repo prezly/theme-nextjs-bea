0e204ebbf6d36416a48b293493ab7e66e6b2bd33
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-08 15:56:26 +0200

    [CARE-3731] Fix robots.txt being inaccessible

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,6 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          */
-        '/((?!api|_next/static|_next/image|favicon\\.ico|sitemap\\.xml).*)',
+        '/((?!robots.txt|api|_next/static|_next/image|favicon\\.ico|sitemap\\.xml).*)',
     ],
 };
