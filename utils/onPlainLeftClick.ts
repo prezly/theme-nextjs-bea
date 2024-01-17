2da626dfa4f0ac5e8f4bab9d367d0fd764923f09
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-17 15:15:40 +0200

    [DEV-12325] Fix ESLint complaint

--- utils/onPlainLeftClick.ts
+++ utils/onPlainLeftClick.ts
@@ -11,7 +11,7 @@ export function onPlainLeftClick<T extends HTMLElement>(
             !event.metaKey &&
             !event.shiftKey
         ) {
-            return callback?.(event);
+            callback?.(event);
         }
     };
 }
