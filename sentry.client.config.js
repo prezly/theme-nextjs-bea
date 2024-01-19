be1764a4ab799759da3216361b382fa6368d921f
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2024-01-19 14:25:52 +0200

    [CARE-3732] Update theme codename in Sentry config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,5 +8,5 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 if (SENTRY_DSN) {
-    Sentry.init(buildSentryClientConfig(SENTRY_DSN, 'bea'));
+    Sentry.init(buildSentryClientConfig(SENTRY_DSN, 'bea-next'));
 }
