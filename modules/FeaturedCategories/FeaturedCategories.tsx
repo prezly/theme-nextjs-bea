2d932f4675e9a4d21411f71619530e5e2675ae23
pestaa <istvan@prezly.com>
2024-03-22 02:22:23 +0100

    [DEV-12659] Develop featured categories homepage block: skip empty categories

--- modules/FeaturedCategories/FeaturedCategories.tsx
+++ modules/FeaturedCategories/FeaturedCategories.tsx
@@ -12,7 +12,7 @@ export async function FeaturedCategories({ localeCode }: Props) {
     const categories = await app().categories();
     const translatedCategories = await app().translatedCategories(
         localeCode,
-        categories.filter((i) => i.is_featured),
+        categories.filter((i) => i.is_featured && i.i18n[localeCode]?.public_stories_number > 0),
     );
 
     if (translatedCategories.length === 0) {
