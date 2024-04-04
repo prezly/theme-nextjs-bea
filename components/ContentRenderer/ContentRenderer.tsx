4ae7b74eea534f94c73e7f914a2463ed9c9879e3
kudlajz <kudlajzzz@gmail.com>
2024-04-04 18:58:45 +0200

    Remove usage of Embed and Video components for now

--- components/ContentRenderer/ContentRenderer.tsx
+++ components/ContentRenderer/ContentRenderer.tsx
@@ -4,7 +4,6 @@ import type { Node } from '@prezly/story-content-format';
 import {
     AttachmentNode,
     ButtonBlockNode,
-    EmbedNode,
     GalleryNode,
     HeadingNode,
     HtmlNode,
@@ -17,7 +16,6 @@ import {
     QuoteNode,
     StoryBookmarkNode,
     VariableNode,
-    VideoNode,
 } from '@prezly/story-content-format';
 
 import {
@@ -34,14 +32,12 @@ import {
 import { AttachBodyClass } from './AttachBodyClass';
 import {
     Attachment,
-    Embed,
     Gallery,
     Image,
     StoryBookmark,
     StoryBookmarkContextProvider,
     Variable,
     VariableContextProvider,
-    Video,
 } from './components';
 
 import styles from './ContentRenderer.module.scss';
@@ -62,7 +58,6 @@ export function ContentRenderer({ nodes, story }: Props) {
                     match={ButtonBlockNode.isButtonBlockNode}
                     component={Elements.ButtonBlock}
                 />
-                <Component match={EmbedNode.isEmbedNode} component={Embed} />
                 <Component match={GalleryNode.isGalleryNode} component={Gallery} />
                 {/* Title and Subtitle heading rules must be defined above the general Heading */}
                 <Component match={HeadingNode.isTitleHeadingNode} component={Elements.Ignore} />
@@ -77,7 +72,6 @@ export function ContentRenderer({ nodes, story }: Props) {
                 <Component match={ParagraphNode.isParagraphNode} component={Paragraph} />
                 <Component match={QuoteNode.isQuoteNode} component={Quote} />
                 <Component match={VariableNode.isVariableNode} component={Variable} />
-                <Component match={VideoNode.isVideoNode} component={Video} />
                 <Component
                     match={StoryBookmarkNode.isStoryBookmarkNode}
                     component={StoryBookmark}
