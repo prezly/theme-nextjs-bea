cb27144b43709de659ea02b2e81a7fe2c2788a72
kudlajz <kudlajzzz@gmail.com>
2024-04-11 17:41:08 +0200

    Also track image/gallery image views

--- components/ContentRenderer/components/Gallery.tsx
+++ components/ContentRenderer/components/Gallery.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { DOWNLOAD, useAnalytics } from '@prezly/analytics-nextjs';
+import { DOWNLOAD, useAnalytics, VIEW } from '@prezly/analytics-nextjs';
 import { Elements } from '@prezly/content-renderer-react-js';
 import type { GalleryNode } from '@prezly/story-content-format';
 
@@ -16,6 +16,9 @@ export function Gallery({ node }: Props) {
             node={node}
             onImageDownload={(image) => {
                 track(DOWNLOAD.GALLERY_IMAGE, { id: image.uuid });
+            }}
+            onPreviewOpen={(image) => {
+                track(VIEW.GALLERY_IMAGE, { id: image.uuid });
             }}
         />
     );
