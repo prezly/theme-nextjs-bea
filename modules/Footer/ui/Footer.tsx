cffa820f6070c970e567e1519f7ea454521d401a
kudlajz <kudlajzzz@gmail.com>
2024-07-01 16:06:21 +0200

    Add support for branding preview

--- modules/Footer/ui/Footer.tsx
+++ modules/Footer/ui/Footer.tsx
@@ -1,21 +1,36 @@
-import type { ReactNode } from 'react';
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { type ReactNode, useMemo } from 'react';
 
 import { MadeWithPrezly } from '@/components/MadeWithPrezly';
+import { parseBoolean } from 'utils';
 
 import styles from './Footer.module.scss';
 
 interface Props {
     children: ReactNode;
-    isWhiteLabel: boolean;
+    isWhiteLabeled: boolean;
 }
 
-export function Footer({ children, isWhiteLabel }: Props) {
+export function Footer({ children, ...props }: Props) {
+    const searchParams = useSearchParams();
+
+    const isWhiteLabeled = useMemo(() => {
+        const isWhiteLabeledPreview = searchParams.get('is_white_labeled');
+        if (isWhiteLabeledPreview) {
+            return parseBoolean(isWhiteLabeledPreview);
+        }
+
+        return props.isWhiteLabeled;
+    }, [props.isWhiteLabeled, searchParams]);
+
     return (
         <footer className={styles.container}>
             <div className="container">
                 <div className={styles.footer}>
                     <div className={styles.links}>{children}</div>
-                    {!isWhiteLabel && <MadeWithPrezly />}
+                    {!isWhiteLabeled && <MadeWithPrezly />}
                 </div>
             </div>
         </footer>
