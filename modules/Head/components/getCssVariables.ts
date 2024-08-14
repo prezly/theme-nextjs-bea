0b66651582cefdfc4c56621d727692cc135daf20
kudlajz <kudlajzzz@gmail.com>
2024-08-14 15:28:51 +0200

    Add and use more variables throughout the theme

--- modules/Head/components/getCssVariables.ts
+++ modules/Head/components/getCssVariables.ts
@@ -44,9 +44,14 @@ export function getCssVariables(settings: ThemeSettings, defaults: ThemeSettings
         ? tinycolor(background_color).darken(accentVariationFactors.DARK)
         : tinycolor(background_color).lighten(accentVariationFactors.LIGHT);
 
-    const textColorHover = tinycolor(text_color).isLight()
-        ? tinycolor(text_color).lighten(accentVariationFactors.LIGHT)
-        : tinycolor(text_color).darken(accentVariationFactors.DARK);
+    const textColorHover = tinycolor(text_color).darken(accentVariationFactors.DARK);
+    const textColorMuted = tinycolor(text_color).isLight()
+        ? tinycolor(text_color).darken(accentVariationFactors.DARKEST)
+        : tinycolor(text_color).lighten(accentVariationFactors.LIGHTEST);
+
+    const borderColor = tinycolor(text_color).isDark()
+        ? tinycolor(text_color).lighten(60)
+        : tinycolor(text_color).darken(60);
 
     const footerTextColorVariation = tinycolor(footer_text_color).isLight()
         ? tinycolor(footer_text_color).darken(accentVariationFactors.DARK)
@@ -54,29 +59,29 @@ export function getCssVariables(settings: ThemeSettings, defaults: ThemeSettings
 
     return {
         '--prezly-font-family': getFontFamily(font),
+        '--prezly-border-color': borderColor,
         '--prezly-text-color': text_color,
-        '--prezly-text-color-hover': textColorHover.toHexString(),
+        '--prezly-text-color-hover': textColorHover,
+        '--prezly-text-color-muted': textColorMuted,
         '--prezly-background-color': background_color,
-        '--prezly-background-color-variation': backgroundColorVariation.toHexString(),
+        '--prezly-background-color-variation': backgroundColorVariation,
         '--prezly-accent-color': accent_color,
-        '--prezly-accent-color-light': tinycolor(accent_color)
-            .lighten(accentVariationFactors.LIGHT)
-            .toHexString(),
-        '--prezly-accent-color-lighter': tinycolor(accent_color)
-            .lighten(accentVariationFactors.LIGHTER)
-            .toHexString(),
-        '--prezly-accent-color-lightest': tinycolor(accent_color)
-            .lighten(accentVariationFactors.LIGHTEST)
-            .toHexString(),
-        '--prezly-accent-color-dark': tinycolor(accent_color)
-            .darken(accentVariationFactors.DARK)
-            .toHexString(),
-        '--prezly-accent-color-darker': tinycolor(accent_color)
-            .darken(accentVariationFactors.DARKER)
-            .toHexString(),
-        '--prezly-accent-color-darkest': tinycolor(accent_color)
-            .darken(accentVariationFactors.DARKEST)
-            .toHexString(),
+        '--prezly-accent-color-light': tinycolor(accent_color).lighten(
+            accentVariationFactors.LIGHT,
+        ),
+        '--prezly-accent-color-lighter': tinycolor(accent_color).lighten(
+            accentVariationFactors.LIGHTER,
+        ),
+        '--prezly-accent-color-lightest': tinycolor(accent_color).lighten(
+            accentVariationFactors.LIGHTEST,
+        ),
+        '--prezly-accent-color-dark': tinycolor(accent_color).darken(accentVariationFactors.DARK),
+        '--prezly-accent-color-darker': tinycolor(accent_color).darken(
+            accentVariationFactors.DARKER,
+        ),
+        '--prezly-accent-color-darkest': tinycolor(accent_color).darken(
+            accentVariationFactors.DARKEST,
+        ),
         '--prezly-accent-color-button-text': accentColorButtonText,
         '--prezly-header-background-color': header_background_color,
         '--prezly-header-link-color': header_link_color,
@@ -84,7 +89,7 @@ export function getCssVariables(settings: ThemeSettings, defaults: ThemeSettings
             header_link_color === defaults.header_link_color ? '#1f2937' : header_link_color,
         '--prezly-footer-background-color': footer_background_color,
         '--prezly-footer-text-color': footer_text_color,
-        '--prezly-footer-text-color-variation': footerTextColorVariation.toHexString(),
+        '--prezly-footer-text-color-variation': footerTextColorVariation,
         '--prezly-placeholder-background-color': placeholderBackgroundColor,
     };
 }
