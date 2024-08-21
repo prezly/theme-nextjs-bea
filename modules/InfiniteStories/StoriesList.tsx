6a7566be9d01885561a31c2e7a1f4a9e499d07f6
kudlajz <kudlajzzz@gmail.com>
2024-08-21 13:34:24 +0200

    Tweaked layout

--- modules/InfiniteStories/StoriesList.tsx
+++ modules/InfiniteStories/StoriesList.tsx
@@ -78,7 +78,7 @@ export function StoriesList({
     return (
         <>
             {highlightedStories.length > 0 && (
-                <div className={styles.highlightedStoriesContainer}>
+                <div>
                     {highlightedStories.map((story) => (
                         <HighlightedStoryCard
                             key={story.uuid}
