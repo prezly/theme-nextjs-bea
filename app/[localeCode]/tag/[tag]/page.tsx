93b1be44afe43bb9b139b919e14e35527a9805f1
Francis Gyimah <fgyimah883@gmail.com>
2024-11-05 09:26:23 +0000

    [CARE-6533] Added tag page to theme

---
+++ app/[localeCode]/tag/[tag]/page.tsx
@@ -0,0 +1,42 @@
+import type { Locale } from '@prezly/theme-kit-nextjs';
+import type { Metadata } from 'next';
+
+import { app, generatePageMetadata, routing } from '@/adapters/server';
+import { Tag as TagIndexPage } from '@/modules/Tag';
+import { getStoryListPageSize, parsePreviewSearchParams } from 'utils';
+
+interface Props {
+    params: {
+        localeCode: Locale.Code;
+        tag: NonNullable<string>;
+    };
+    searchParams: Record<string, string>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { generateUrl } = await routing();
+    const { localeCode, tag } = params;
+
+    return generatePageMetadata({
+        locale: localeCode,
+        title: tag,
+        generateUrl: (locale) => generateUrl('tag', { localeCode: locale, tag }),
+    });
+}
+
+export default async function TagPage({ params, searchParams }: Props) {
+    const themeSettings = await app().themeSettings();
+    const settings = parsePreviewSearchParams(searchParams, themeSettings);
+
+    return (
+        <TagIndexPage
+            locale={params.localeCode}
+            tag={params.tag}
+            layout={settings.layout}
+            pageSize={getStoryListPageSize(settings.layout)}
+            showDate={settings.show_date}
+            showSubtitle={settings.show_subtitle}
+            storyCardVariant={settings.story_card_variant}
+        />
+    );
+}
