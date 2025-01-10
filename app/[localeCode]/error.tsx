11b8179670ef830b440f4efa9099d4ebf27c9d92
Francis Gyimah <fgyimah883@gmail.com>
2025-01-10 13:59:58 +0000

    [DEV-19082] Updated error page to use new Error module

--- app/[localeCode]/error.tsx
+++ app/[localeCode]/error.tsx
@@ -1,46 +1,7 @@
-/* eslint-disable id-blacklist */
-
 'use client';
 
-import Illustration from '@/public/images/error.svg';
-
-import 'modern-normalize/modern-normalize.css';
-import '@/styles/styles.globals.scss';
-
-import styles from './error.module.scss';
+import { Error as ErrorComponent } from '@/modules/Error';
 
 export default function Error() {
-    function handlePageRefresh() {
-        window.location.reload();
-    }
-
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div className={styles.illustration}>
-                    <Illustration className={styles.svg} />
-                </div>
-                <div className={styles.content}>
-                    <h1 className={styles.title}>We’re sorry, this site couldn’t load</h1>
-                    <p className={styles.description}>
-                        Try to{' '}
-                        <button className={styles.link} onClick={handlePageRefresh}>
-                            refresh
-                        </button>{' '}
-                        the page, or visit the platform’s{' '}
-                        <a className={styles.link} href="https://status.prezly.com">
-                            status page
-                        </a>{' '}
-                        to see if any incidents are affecting the platform. You can also reach out
-                        to support via{' '}
-                        <a className={styles.link} href="mailto:support@prezly.com">
-                            {' '}
-                            email
-                        </a>
-                        .
-                    </p>
-                </div>
-            </div>
-        </div>
-    );
+    return <ErrorComponent />;
 }
