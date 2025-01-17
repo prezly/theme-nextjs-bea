1b15528b0f7825fad7999fabd247f81ac2e0c840
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-17 15:53:02 +0700

    Update CookieConsentProvider

--- modules/CookieConsent/CookieConsentContext.tsx
+++ modules/CookieConsent/CookieConsentContext.tsx
@@ -1,28 +1,61 @@
 'use client';
 
+import { Newsroom } from '@prezly/sdk';
 import type { Dispatch, ReactNode, SetStateAction } from 'react';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useRef, useState } from 'react';
 
 import type { Consent } from './types';
 
 interface Context {
     consent: Consent | null;
+    trackingPolicy: Newsroom['tracking_policy'];
     setConsent: Dispatch<SetStateAction<Consent | null>>;
+    updatePreferences: () => void;
+    isNavigatorSupportsCookies: boolean;
+    registerUpdatePreferencesCallback: (callback: () => undefined) => void;
 }
 
 export const context = createContext<Context>({
     consent: null,
     setConsent: () => undefined,
+    trackingPolicy: Newsroom.TrackingPolicy.DEFAULT,
+    updatePreferences: () => undefined,
+    isNavigatorSupportsCookies: true,
+    registerUpdatePreferencesCallback: () => undefined,
 });
 
 interface Props {
     children: ReactNode;
+    trackingPolicy: Newsroom['tracking_policy'];
 }
 
-export function CookieConsentProvider({ children }: Props) {
+export function CookieConsentProvider({ children, trackingPolicy }: Props) {
     const [consent, setConsent] = useState<Consent | null>(null);
+    const updatePreferencesCallbackRef = useRef<null | (() => void)>(null);
+    const isNavigatorSupportsCookies = typeof navigator !== 'undefined' && navigator.cookieEnabled;
 
-    return <context.Provider value={{ consent, setConsent }}>{children}</context.Provider>;
+    const updatePreferences = () => {
+        updatePreferencesCallbackRef.current?.();
+    };
+
+    function registerUpdatePreferencesCallback(callback: () => void) {
+        updatePreferencesCallbackRef.current = callback;
+    }
+
+    return (
+        <context.Provider
+            value={{
+                consent,
+                isNavigatorSupportsCookies,
+                trackingPolicy,
+                setConsent,
+                updatePreferences,
+                registerUpdatePreferencesCallback,
+            }}
+        >
+            {children}
+        </context.Provider>
+    );
 }
 
 export function useCookieConsent() {
