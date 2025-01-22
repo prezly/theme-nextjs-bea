d2a4ba45c3bc8a60ae10a4bc984629dfefed9913
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-22 20:16:56 +0700

    Export sharing-related lucide icons

--- icons/index.ts
+++ icons/index.ts
@@ -1,3 +1,9 @@
+export {
+    Link as IconLink,
+    FileDown as IconFileDown,
+    FolderDown as IconFolderDown,
+    Text as IconText,
+} from 'lucide-react';
 export * from './file-types';
 export * from './generic';
 export * from './social';
