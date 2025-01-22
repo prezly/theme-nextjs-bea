855477cc52e66a1a26266fae8b24322bde756c43
Francis Gyimah <fgyimah883@gmail.com>
2025-01-22 13:32:43 +0000

    [DEV-19203] Updated deprecated components & usage

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -9,12 +9,12 @@ export function useDebounce<T extends (...params: never[]) => void>(milliseconds
     // @see https://github.com/facebook/react/issues/16956#issuecomment-536636418
     fnRef.current = fn;
 
-    const timer = useRef<number>();
+    const timer = useRef<number>(null);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
     return useCallback<T>(
         ((...params) => {
-            clearTimeout(timer.current);
+            clearTimeout(timer.current ?? undefined);
             setTimeout(() => fnRef.current(...params), milliseconds);
         }) as T,
         [fnRef, timer, milliseconds],
