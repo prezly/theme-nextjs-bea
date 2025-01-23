610b8fc0632e73d0aa79ec3f777daa6d6f249563
Francis Gyimah <fgyimah883@gmail.com>
2025-01-23 10:19:59 +0000

    [DEV-19203] Updated dirs in next.config.js

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const moduleExports = withBundleAnalyzer(
                 'icons',
                 'modules',
                 'pages',
-                '@/utils',
+                'utils',
                 'ui',
             ],
         },
