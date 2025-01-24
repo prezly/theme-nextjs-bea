a31565c0e3801eac028c67ca4d39dc1c70900e3b
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-01-24 12:58:30 +0700

    Fix useEffect dependency

--- modules/CookieConsent/components/VanillaCookieConsent/VanillaCookieConsent.tsx
+++ modules/CookieConsent/components/VanillaCookieConsent/VanillaCookieConsent.tsx
@@ -105,7 +105,7 @@ export function VanillaCookieConsent({ cookieStatement }: Props) {
         registerUpdatePreferencesCallback(() => {
             CookieConsent.showPreferences();
         });
-    });
+    }, [registerUpdatePreferencesCallback]);
 
     return null;
 }
