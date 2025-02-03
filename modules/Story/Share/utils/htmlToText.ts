34b92fb14a2eacec4e2178526d810c458729cd3d
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-03 16:05:24 +0700

    Update htmlToText configuration

--- modules/Story/Share/utils/htmlToText.ts
+++ modules/Story/Share/utils/htmlToText.ts
@@ -4,6 +4,8 @@ import { convert } from 'html-to-text';
 
 export async function htmlToText(html: string) {
     return convert(html, {
+        preserveNewlines: true,
+        wordwrap: false,
         selectors: [
             { selector: 'a', format: 'inline' },
             { selector: 'img', format: 'skip' },
