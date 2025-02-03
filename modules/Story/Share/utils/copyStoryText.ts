043c70a68c042b7c6c8b57be9cc8eaa5d0cc3c2f
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-03 16:06:38 +0700

    Support text wrapping in text-based tables copying

--- modules/Story/Share/utils/copyStoryText.ts
+++ modules/Story/Share/utils/copyStoryText.ts
@@ -7,6 +7,7 @@ type Attribute = string;
 const ELEMENTS_ATTRIBUTES_WHITELIST: Record<TagName, Attribute[]> = {
     A: ['href', 'target'],
 };
+const MAX_DESIRED_CELL_WIDTH = 30;
 
 export async function copyStoryText(articleElementSelector = DEFAULT_ARTICLE_SELECTOR) {
     const element = document.querySelector<HTMLElement>(articleElementSelector);
@@ -46,7 +47,7 @@ async function getArticleText(element: HTMLElement): Promise<string> {
     const clonedElement = element.cloneNode(true) as HTMLElement;
 
     clonedElement.querySelectorAll('table').forEach((node) => {
-        node.outerHTML = `<div>${htmlTableToPlainText(node)}</div>`;
+        node.textContent = htmlTableToPlainText(node);
     });
 
     return htmlToText(clonedElement.innerHTML);
@@ -77,18 +78,67 @@ function htmlTableToPlainText(tableElement: HTMLTableElement): string {
 
     const columnWidths: number[] = rows.reduce<number[]>(
         (result, row) =>
-            Array.from(row.cells, (cell, index) =>
-                Math.max(result[index] || 0, cell.textContent?.trim().length || 0),
-            ),
+            Array.from(row.cells, (cell, index) => {
+                const text = cell.textContent!.trim()!;
+                const maxWordLength = Math.max(...text.split(/\s/g).map((word) => word.length));
+                const cellWidth =
+                    text.length <= MAX_DESIRED_CELL_WIDTH
+                        ? text.length
+                        : Math.max(MAX_DESIRED_CELL_WIDTH, maxWordLength);
+
+                return Math.max(result[index] || 0, cellWidth);
+            }),
         [],
     );
 
-    const plainTextRows = rows.map((row) =>
+    const rowsLines = rows.map((row) =>
         Array.from(row.cells, (cell, index) => {
-            const content = cell.textContent?.trim() || '';
-            return content.padEnd(columnWidths[index], '\xa0'); // &nbsp;
-        }).join(' | '),
+            const text = cell.textContent?.trim() || '';
+            return wrapText(text, columnWidths[index]);
+        }),
     );
 
-    return plainTextRows.join('<br/>');
+    const columnSeparator = '\xa0│\xa0'; // &nbsp;
+    const rowLength =
+        columnWidths.reduce((sum, columnWidth) => sum + columnWidth) +
+        columnSeparator.length * (columnWidths.length - 1);
+    const separator = `\n${'─'.repeat(rowLength)}\n`;
+
+    return rowsLines
+        .flatMap((row) => {
+            const rowHeight = Math.max(...row.map((text) => text.split('\n').length));
+
+            return Array.from({ length: rowHeight }, (_, lineIndex) => {
+                return row
+                    .map((cell, columnIndex) => {
+                        const lineText = cell.split('\n')[lineIndex] ?? '';
+                        return lineText.padEnd(columnWidths[columnIndex], '\xa0'); // &nbsp;
+                    })
+                    .join(columnSeparator);
+            }).join('\n');
+        })
+        .join(separator);
+}
+
+function wrapText(text: string, maxLineLength: number): string {
+    if (text.length < maxLineLength) {
+        return text;
+    }
+
+    let currentLineLength = 0;
+
+    return text
+        .split(/\s/g)
+        .map((word) => {
+            const shouldAddSpace = currentLineLength > 0;
+
+            if (currentLineLength + word.length + (shouldAddSpace ? 1 : 0) > maxLineLength) {
+                currentLineLength = word.length;
+                return `\n${word}`;
+            }
+
+            currentLineLength += word.length + (shouldAddSpace ? 1 : 0);
+            return (shouldAddSpace ? ' ' : '') + word;
+        })
+        .join('');
 }
