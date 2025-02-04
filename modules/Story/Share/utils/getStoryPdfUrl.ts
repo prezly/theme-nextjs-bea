3183ba35b06251f732b238327910db3d82d3c096
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-05 02:18:54 +0700

    Fix `getStoryPdfUrl` util

--- modules/Story/Share/utils/getStoryPdfUrl.ts
+++ modules/Story/Share/utils/getStoryPdfUrl.ts
@@ -1,22 +1,15 @@
 'use server';
 
-import { createPrezlyClient, routing, type Story } from '@prezly/sdk';
+import { routing, type Story } from '@prezly/sdk';
 
-const prezlyClient = createPrezlyClient({
-    accessToken: process.env.PREZLY_ACCESS_TOKEN!,
-    baseUrl: process.env.PREZLY_API_BASEURL!,
-});
+const { PREZLY_ACCESS_TOKEN, PREZLY_API_BASEURL } = process.env;
 
 export async function getStoryPdfUrl(uuid: Story['uuid']) {
-    prezlyClient.api
-        .get(`${routing.storiesUrl}/${uuid}`, {
-            headers: {
-                Accept: 'application/pdf',
-            },
-        })
-        .then(console.log)
-        .catch(console.log);
-
-    // TODO: return a proper PDF url
-    return '';
+    return fetch(`${PREZLY_API_BASEURL}${routing.storiesUrl}/${uuid}`, {
+        headers: {
+            Authorization: `Bearer ${PREZLY_ACCESS_TOKEN}`,
+            Accept: 'application/pdf',
+        },
+        redirect: 'manual',
+    }).then((response) => response.headers.get('location'));
 }
