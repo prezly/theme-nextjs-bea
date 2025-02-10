7bdfce58126278c1fdd0562a3c55944b02304714
Yuriy Yakym <yakym.yuriy@gmail.com>
2025-02-10 19:10:13 +0700

    Add privacy-policy page

---
+++ app/[localeCode]/(policies)/privacy-policy/page.tsx
@@ -0,0 +1,48 @@
+import type { Locale } from '@prezly/theme-kit-nextjs';
+import { redirect } from 'next/navigation';
+
+import { app, generatePageMetadata } from '@/adapters/server';
+import { ContentRenderer } from '@/components/ContentRenderer';
+import { PageTitle } from '@/components/PageTitle';
+
+interface Props {
+    params: {
+        localeCode: Locale.Code;
+        slug: string;
+    };
+    searchParams: Record<string, string>;
+}
+
+export async function generateMetadata({ params }: Props) {
+    const { policies } = await app().newsroom();
+    const { cookie_policy: policy } = policies;
+
+    if ('link' in policy) {
+        redirect(policy.link);
+    }
+
+    return generatePageMetadata({
+        locale: params.localeCode,
+        title: 'Privacy Policy',
+        description:
+            'We believe in protecting your privacy. This Privacy Policy explains how your information is protected, stored and used.',
+    });
+}
+
+export default async function StoryPage({}: Props) {
+    const { policies } = await app().newsroom();
+    const { privacy_policy: policy } = policies;
+
+    if ('link' in policy) {
+        redirect(policy.link);
+    }
+
+    const content = JSON.parse(policy.content);
+
+    return (
+        <>
+            <PageTitle title="Privacy Policy" />
+            <ContentRenderer nodes={content} />
+        </>
+    );
+}
