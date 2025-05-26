2b24166eaa7137625ae13e2b7bc9fc1592ce3118
kudlajz <kudlajzzz@gmail.com>
2025-05-26 12:58:38 +0200

    Destructure properties for simpler code

--- src/modules/InfiniteHubStories/NewsroomLogo.tsx
+++ src/modules/InfiniteHubStories/NewsroomLogo.tsx
@@ -11,39 +11,33 @@ type Props = {
 };
 
 export function NewsroomLogo({ newsroom }: Props) {
-    const { newsroom_logo, square_logo } = newsroom;
-    const image =
-        getUploadcareImage(newsroom.square_logo) ?? getUploadcareImage(newsroom.newsroom_logo);
+    const { active_theme_preset, display_name, newsroom_logo, square_logo, url } = newsroom;
+    const image = getUploadcareImage(square_logo) ?? getUploadcareImage(newsroom_logo);
 
     const isSquareLogo = square_logo !== null;
     const isMainLogo = !isSquareLogo && newsroom_logo !== null;
 
     return (
         <a
-            href={newsroom.url}
+            href={url}
             className={classNames(styles.newsroom, {
                 [styles.withPadding]: isMainLogo || !image,
             })}
             target="_blank"
-            title={`Go to site ${newsroom.display_name}`}
+            title={`Go to site ${display_name}`}
             style={
                 isMainLogo
                     ? {
-                          color: newsroom.active_theme_preset.settings.text_color,
-                          backgroundColor: newsroom.active_theme_preset.settings.background_color,
+                          color: active_theme_preset.settings.header_link_color,
+                          backgroundColor: active_theme_preset.settings.header_background_color,
                       }
                     : undefined
             }
         >
             {image ? (
-                <UploadcareImage
-                    alt={newsroom.display_name}
-                    src={image.cdnUrl}
-                    width={373}
-                    height={373}
-                />
+                <UploadcareImage alt={display_name} src={image.cdnUrl} width={373} height={373} />
             ) : (
-                newsroom.display_name
+                display_name
             )}
         </a>
     );
