c8476c5af7e24ea3ce1b0ceca2210a657924d804
kudlajz <kudlajzzz@gmail.com>
2025-06-16 12:58:33 +0200

    Do not reflect search state in URL

--- src/modules/Search/Search.tsx
+++ src/modules/Search/Search.tsx
@@ -2,8 +2,7 @@
 
 import type { Newsroom } from '@prezly/sdk';
 import type { Locale } from '@prezly/theme-kit-nextjs';
-import { useDebouncedCallback } from '@react-hookz/web';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useMemo, useState } from 'react';
 import { Configure, InstantSearch } from 'react-instantsearch-dom';
 
@@ -17,9 +16,9 @@ import SearchStateContextProvider from './components/SearchStateContext';
 import { Subtitle } from './components/Subtitle';
 import { Title } from './components/Title';
 import type { SearchState } from './types';
-import { createUrl, queryToSearchState, searchStateToQuery } from './utils';
+import { createUrl, queryToSearchState } from './utils';
 
-const DEBOUNCE_TIME_MS = 300;
+// const DEBOUNCE_TIME_MS = 300;
 
 interface Props {
     localeCode: Locale.Code;
@@ -41,24 +40,24 @@ export function Search({
     storyCardVariant,
 }: Props) {
     const query = useSearchParams();
-    const { replace } = useRouter();
+    // const { replace } = useRouter();
     const [searchState, setSearchState] = useState<SearchState>(queryToSearchState(query));
 
     const searchClient = useMemo(() => getSearchClient(settings), [settings]);
 
     const filters = `attributes.culture.code=${localeCode}`;
 
-    const scheduleUrlUpdate = useDebouncedCallback(
-        (updatedSearchState: SearchState) => {
-            replace(`?${searchStateToQuery(updatedSearchState)}`);
-        },
-        [replace, DEBOUNCE_TIME_MS],
-        DEBOUNCE_TIME_MS,
-    );
+    // const scheduleUrlUpdate = useDebouncedCallback(
+    //     (updatedSearchState: SearchState) => {
+    //         replace(`?${searchStateToQuery(updatedSearchState)}`);
+    //     },
+    //     [replace, DEBOUNCE_TIME_MS],
+    //     DEBOUNCE_TIME_MS,
+    // );
 
     function onSearchStateChange(updatedSearchState: SearchState) {
         setSearchState(updatedSearchState);
-        scheduleUrlUpdate(updatedSearchState);
+        // scheduleUrlUpdate(updatedSearchState);
     }
 
     return (
