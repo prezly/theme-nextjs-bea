0cc89be62675d7b29a4eeee98cfdb19f12521978
Francis Gyimah <fgyimah883@gmail.com>
2025-11-18 11:37:08 +0000

    [DEV-22040] Added IconSettings from lucide

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,9 +4,10 @@ export {
     FileDown as IconFileDown,
     FolderDown as IconFolderDown,
     Text as IconText,
-} from 'lucide-react';
-export * from './file-types';
-export * from './generic';
-export * from './social';
+    Settings as IconSettings,
+} from "lucide-react";
+export * from "./file-types";
+export * from "./generic";
+export * from "./social";
 
-export type { IconComponentType } from './types';
+export type { IconComponentType } from "./types";
