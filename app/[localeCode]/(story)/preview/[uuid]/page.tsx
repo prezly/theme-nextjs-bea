92be49ae8f00e5defe6584b30ab4618466939396
Francis Gyimah <fgyimah883@gmail.com>
2025-11-23 22:42:51 +0000

    [DEV-22064] Passed has related stories to Story component
    - Dropped redundant locale prop

--- app/[localeCode]/(story)/preview/[uuid]/page.tsx
+++ app/[localeCode]/(story)/preview/[uuid]/page.tsx
@@ -43,7 +43,6 @@ export async function generateMetadata(props: Props) {
 }
 
 export default async function PreviewStoryPage(props: Props) {
-    const { localeCode } = await props.params;
     const searchParams = await props.searchParams;
     const { story, relatedStories } = await resolve(props.params);
     const settings = await app().themeSettings();
@@ -57,6 +56,7 @@ export default async function PreviewStoryPage(props: Props) {
                 showDate={settings.show_date}
                 withHeaderImage={themeSettings.header_image_placement}
                 relatedStories={themeSettings.show_read_more ? relatedStories : []}
+                hasRelatedStories={themeSettings.show_read_more}
                 actions={{
                     show_copy_content: themeSettings.show_copy_content,
                     show_copy_url: false, // Unpublished article has no URL
@@ -68,7 +68,6 @@ export default async function PreviewStoryPage(props: Props) {
                     sharing_actions: [], // Cannot share unpublished article
                 }}
                 withBadges={themeSettings.story_card_variant === 'boxed'}
-                locale={localeCode}
             />
         </>
     );
